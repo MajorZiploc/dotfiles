993ca041a8236be3b16394c7412e0432d7502da8
Manyu <lakemonyew@gmail.com>
2020-12-27 11:40:30 -0600

    Edits choco bootstrapper

--- manyus/.setup/bootstrap.ps1
+++ manyus/.setup/bootstrap.ps1
@@ -2,15 +2,16 @@
 
 # install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco feature enable -n allowGlobalConfirmation
+# the -y on all cinst should remove the need for this global confirmation
+# choco feature enable -n allowGlobalConfirmation
 
 # install git and refresh path
-cinst git
+cinst -y git
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
 # clone this repo
 git init
-git remote add origin https://github.com/mattstermiller/home-settings.git
+git remote add origin https://github.com/MajorZiploc/home-settings
 git fetch
 git reset origin/master
 git branch --set-upstream-to=origin/master
