0cc03f1d35758591875f6da6740a104f4ceeda56
Manyu <lakemonyew@gmail.com>
2020-12-27 11:37:51 -0600

    adds full and min paths to install script

--- manyus/.setup/install.ps1
+++ manyus/.setup/install.ps1
@@ -1,36 +1,47 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [ValidateSet("Full", "Minimal")]
+    [string]
+    $type="Full"
+)
+
 # install apps
-cinst googlechrome
-cinst nodejs
-cinst ditto
-cinst git
-cinst winrar
-cinst swi-prolog
-cinst install python3 --version=3.8.6
-cinst 7zip
-cinst vscode
-cinst gimp
-cinst katmouse
-# cinst slack
+cinst -y googlechrome
+cinst -y ditto
+cinst -y git
+cinst -y winrar
+cinst -y 7zip
+cinst -y katmouse
+cinst -y vscode
+
+if ("Full" -eq $type) {
+    cinst -y gimp
+    cinst -y swi-prolog
+    cinst -y nodejs
+    cinst -y python3 --version=3.8.6
+    cinst -y vscode
+    cinst -y smtp4dev
+    # cinst -y slack
+}
 
 # look into these
-# cinst screentogif
-# cinst logitechgaming
-# cinst vim-tux
-# cinst autohotkey
-# cinst nircmd
-# cinst vlc
-# cinst irfanview
+# cinst -y screentogif
+# cinst -y logitechgaming
+# cinst -y vim-tux
+# cinst -y autohotkey
+# cinst -y nircmd
+# cinst -y vlc
+# cinst -y irfanview
 
 # install dev tools
-# cinst gitkraken
-cinst vscode
-# cinst linqpad5
-# cinst dotnetcore-sdk
-# cinst fake
-#cinst visualstudio2019-workload-manageddesktopbuildtools
-#cinst yarn
-#cinst smtp4dev
-#cinst postman
+# cinst -y gitkraken
+# cinst -y linqpad5
+# cinst -y dotnetcore-sdk
+# cinst -y fake
+#cinst -y visualstudio2019-workload-manageddesktopbuildtools
+#cinst -y yarn
+#cinst -y postman
 
 # generate shims
 # & "$env:ChocolateyInstall\tools\shimgen.exe" -p "$env:ChocolateyInstall\tools\shimgen.exe" -o "$env:ChocolateyInstall\bin\shimgen.exe"
