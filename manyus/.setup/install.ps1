8e62760783152c06c12f63972f3a0df6c0eddcee
Manyu <LakeMonYew@gmail.com>
2020-12-27 09:47:07 -0600

    Adds packages to install script

--- manyus/.setup/install.ps1
+++ manyus/.setup/install.ps1
@@ -1,44 +1,36 @@
 # install apps
 cinst googlechrome
-cinst vim-tux
-cinst arsclip
-cinst katmouse
-cinst autohotkey
-cinst nircmd
-cinst vlc
-cinst irfanview
+cinst nodejs
+cinst ditto
+cinst git
+cinst winrar
+cinst swi-prolog
+cinst install python3 --version=3.8.6
 cinst 7zip
-cinst slack
-#cinst gimp
+# cinst slack
+cinst gimp
 #cinst screentogif
 #cinst logitechgaming
 
+# look into these
+# cinst vim-tux
+# cinst katmouse
+# cinst autohotkey
+# cinst nircmd
+# cinst vlc
+# cinst irfanview
+
 # install dev tools
-cinst gitkraken
+# cinst gitkraken
 cinst vscode
-cinst linqpad5
-cinst dotnetcore-sdk
-cinst fake
+# cinst linqpad5
+# cinst dotnetcore-sdk
+# cinst fake
 #cinst visualstudio2019-workload-manageddesktopbuildtools
 #cinst yarn
 #cinst smtp4dev
 #cinst postman
 
-# install plugins for apps
-git clone https://github.com/VundleVim/Vundle.vim.git ../.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-mkdir ..\.vim\swap
-
-code --install-extension Shan.code-settings-sync
-
 # generate shims
-& "$env:ChocolateyInstall\tools\shimgen.exe" -p "$env:ChocolateyInstall\tools\shimgen.exe" -o "$env:ChocolateyInstall\bin\shimgen.exe"
-shimgen -p "C:\Program Files\Git\git-bash.exe" -o "$env:ChocolateyInstall\bin\git-bash.exe"
-
-# system settings
-& "$env:userprofile\.ahk\hotkeys.ahk"
-nircmd shortcut "$env:userprofile\.ahk\hotkeys.ahk" '~$folder.startup$' "Hotkeys"
-$startMenu = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
-copy "$startMenu\ArsClip.lnk" "$startMenu\Startup"
-
-REGEDIT /S CapsToControl-ScrollToCaps.reg
+# & "$env:ChocolateyInstall\tools\shimgen.exe" -p "$env:ChocolateyInstall\tools\shimgen.exe" -o "$env:ChocolateyInstall\bin\shimgen.exe"
+# shimgen -p "C:\Program Files\Git\git-bash.exe" -o "$env:ChocolateyInstall\bin\git-bash.exe"
