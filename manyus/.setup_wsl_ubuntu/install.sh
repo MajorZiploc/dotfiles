7aada70fa0123aff229ae329e5184e91f8405cc3
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-01 18:13:05 -0600

    updates install script

--- manyus/.setup_wsl_ubuntu/install.sh
+++ manyus/.setup_wsl_ubuntu/install.sh
@@ -1,4 +1,9 @@
 sudo apt -y update
 sudo apt -y upgrade
+# session manager
 sudo apt-get -y install tmux
+# for cr formatting between dos and unix. gives dos2unix and unix2dos
 sudo apt-get -y install dos2unix
+# for clipboard support, note, that the clipboard support doesnt seem to work within wsl using this
+# check vim clipboard with: vim --version | grep clipboard
+sudo apt-get -y install vim-gtk
