1c4f61dd7dfc5557cfd8a238a26770844e5bcbe7
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-20 14:29:55 -0600

    Moves ahk and adds to copy script

--- .setup/scripts/copy.sh
+++ .setup/scripts/copy.sh
@@ -4,6 +4,8 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cp -a "$SCRIPTPATH/../copy_content_to_home/." ~/
 
+cp -a "$SCRIPTPATH/../.ahk" ~/
+
 cp -a "$SCRIPTPATH/../usr_local_bin/." /usr/local/bin/
 
 cp -a "$SCRIPTPATH/../Tasks/." /c/Tasks/
