3a4531d9a3c16f391a4f4a5f22c2e46cfe25b981
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-20 07:06:16 -0600

    Adds more find examples

--- manyus/find_examples.sh
+++ manyus/find_examples.sh
@@ -6,3 +6,17 @@ find . -type f -name '*.mp3' -exec rm -f {} \;
 
 # find all empty directories in folder
 find . -type d -empty
+
+# rename files that match *hid* recursively, need to test more to see how it behaves
+# Maybe? after the rename occurs,
+# the new filename can also matches the pattern. Might not be what you want
+find . -name '*hid*' -exec mv {} {}_renamed \;
+# seems to work better than the previous
+find . -type d -name '*hid*' -print0 | xargs --null -I{} mv {} {}_renamed
+# even better?
+find . -name '*hid*' -printf "%f\0" | xargs --null -I{} mv {} "prefix {} suffix"
+find . -type f -name 'file*' -execdir mv {} {}_renamed ';'
+# okdir will do the changes interactively and ask if you want to perform the exec
+find . -type f -name 'file*' -okdir mv {} {}_renamed ';'
+for i in $(find . -name '*'); do echo $i $i.bak; done
+
