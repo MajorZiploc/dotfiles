02a11d3a092251563a1a6ec86d6c6afddce089f5
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-20 15:11:05 -0600

    Fixes ubuntu copy script

--- .setup_wsl_ubuntu/scripts/copy.sh
+++ .setup_wsl_ubuntu/scripts/copy.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cp -a "$SCRIPTPATH/../copy_content_to_home/." ~/
-la ~ | xargs dos2unix
+ls -A ~ | xargs dos2unix
 
-cp -a "$SCRIPTPATH/../usr_local_bin/." /usr/local/bin/
-la ~ | xargs /usr/local/bin/
+sudo cp -a "$SCRIPTPATH/../usr_local_bin/." /usr/local/bin/
+sudo ls -A /usr/local/bin | xargs sudo dos2unix
