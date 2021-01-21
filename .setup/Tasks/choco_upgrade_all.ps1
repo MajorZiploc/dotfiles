65f8c1a227ca408731474b73d13cb17ad071a620
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-21 10:57:15 -0600

    Adds event logs checks

--- .setup/Tasks/choco_upgrade_all.ps1
+++ .setup/Tasks/choco_upgrade_all.ps1
@@ -1,34 +1,45 @@
+$logName = 'choco'
+$logSource = 'upgrade'
 function Upgrade-Choco {
   cup all -y --except="python3"
 }
 
 function Program {
   param ()
-begin {
-  $msg = "Started process"
-  Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType Information -EventId 1 -Message $msg
-}
 
 process {
 try {
+  # Event log checks and initialization
+  # Ref: http://msdn.microsoft.com/en-us/library/system.diagnostics.eventlog.exists(v=vs.110).aspx
+  # Check if Log exists
+  $doesAppExist = [System.Diagnostics.EventLog]::Exists($logName);
+  # Ref: http://msdn.microsoft.com/en-us/library/system.diagnostics.eventlog.sourceexists(v=vs.110).aspx
+  # Check if Source exists
+  $doesSourceExist = [System.Diagnostics.EventLog]::SourceExists($logSource);
+  if ( !$doesAppExist -or !$doesSourceExist ) {
+    New-EventLog -LogName $logName -Source $logSource
+  }
+
+  $msg = "Started process"
+  Write-EventLog -LogName $logName -Source $logSource -EntryType Information -EventId 1 -Message $msg
   Upgrade-Choco -ErrorAction SilentlyContinue -ErrorVariable ProcessError
   if ($ProcessError) {
     $msg = "Something went wrong. $ProcessError"
-    Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType FailureAudit -EventId 1 -Message $msg
+    Write-EventLog -LogName $logName -Source $logSource -EntryType FailureAudit -EventId 1 -Message $msg
   } else {
     $msg = "Successful upgrade"
-    Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType SuccessAudit -EventId 1 -Message $msg
+    Write-EventLog -LogName $logName -Source $logSource -EntryType SuccessAudit -EventId 1 -Message $msg
   }
 }
 catch {
   $msg = $_.ToString()
-  Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType FailureAudit -EventId 1 -Message $msg
+  Write-EventLog -LogName $logName -Source $logSource -EntryType FailureAudit -EventId 1 -Message $msg
 }
 }
 
 end {
   $msg = "Finished process"
-  Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType Information -EventId 1 -Message $msg
+  Write-EventLog -LogName $logName -Source $logSource -EntryType Information -EventId 1 -Message $msg
 }
 }
 
--- .setup/Tasks/copy_snippets.ps1
+++ .setup/Tasks/copy_snippets.ps1
@@ -1,3 +1,6 @@
+$logName = 'clipboard'
+$logSource = 'copy'
+
 function Copy-Snippets {
   [CmdletBinding()]
   param ()
@@ -11,26 +14,35 @@ function Copy-Snippets {
 
 function Program {
   param ()
-begin {
-  $msg = "Started process"
-  Write-EventLog -LogName 'clipboard' -Source 'copy' -EntryType Information -EventId 1 -Message $msg
-}
 
 process {
 try {
+  # Event log checks and initialization
+  # Ref: http://msdn.microsoft.com/en-us/library/system.diagnostics.eventlog.exists(v=vs.110).aspx
+  # Check if Log exists
+  $doesAppExist = [System.Diagnostics.EventLog]::Exists($logName);
+  # Ref: http://msdn.microsoft.com/en-us/library/system.diagnostics.eventlog.sourceexists(v=vs.110).aspx
+  # Check if Source exists
+  $doesSourceExist = [System.Diagnostics.EventLog]::SourceExists($logSource);
+  if ( !$doesAppExist -or !$doesSourceExist ) {
+    New-EventLog -LogName $logName -Source $logSource
+  }
+
+  $msg = "Started process"
+  Write-EventLog -LogName $logName -Source $logSource -EntryType Information -EventId 1 -Message $msg
   Copy-Snippets -ErrorAction Stop
   $msg = "Successful copy"
-  Write-EventLog -LogName 'clipboard' -Source 'copy' -EntryType SuccessAudit -EventId 1 -Message $msg
+  Write-EventLog -LogName $logName -Source $logSource -EntryType SuccessAudit -EventId 1 -Message $msg
 }
 catch {
   $msg = $_.ToString()
-  Write-EventLog -LogName 'clipboard' -Source 'copy' -EntryType FailureAudit -EventId 1 -Message $msg
+  Write-EventLog -LogName $logName -Source $logSource -EntryType FailureAudit -EventId 1 -Message $msg
 }
 }
 
 end {
   $msg = "Finished process"
-  Write-EventLog -LogName 'clipboard' -Source 'copy' -EntryType Information -EventId 1 -Message $msg
+  Write-EventLog -LogName $logName -Source $logSource -EntryType Information -EventId 1 -Message $msg
 }
 }
 
