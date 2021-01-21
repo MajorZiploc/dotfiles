516fbc89ba2608018bcad1f477919aeca9594f86
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-21 07:36:47 -0600

    Adds event logs to choc upgrade script

--- .setup/Tasks/choco_upgrade_all.ps1
+++ .setup/Tasks/choco_upgrade_all.ps1
@@ -1,1 +1,35 @@
-cup all -y --except="python3"
+function Upgrade-Choco {
+  cup all -y --except="python3"
+}
+
+function Program {
+  param ()
+begin {
+  $msg = "Started process"
+  Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType Information -EventId 1 -Message $msg
+}
+
+process {
+try {
+  Upgrade-Choco -ErrorAction SilentlyContinue -ErrorVariable ProcessError
+  If ($ProcessError) {
+    $msg = "Something went wrong. $ProcessError"
+    Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType FailureAudit -EventId 1 -Message $msg
+  } else {
+    $msg = "Successful upgrade"
+    Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType SuccessAudit -EventId 1 -Message $msg
+  }
+}
+catch {
+  $msg = $_.ToString()
+  Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType FailureAudit -EventId 1 -Message $msg
+}
+}
+
+end {
+  $msg = "Finished process"
+  Write-EventLog -LogName 'choco' -Source 'upgrade' -EntryType Information -EventId 1 -Message $msg
+}
+}
+
+Program
