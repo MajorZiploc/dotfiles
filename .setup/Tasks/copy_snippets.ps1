10071e83af2f9a0c723f6e35e3ae332692a8f8a6
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-21 05:40:44 -0600

    Adds basic error logging to copy clipboard

--- .setup/Tasks/copy_snippets.ps1
+++ .setup/Tasks/copy_snippets.ps1
@@ -1,7 +1,24 @@
 
-[string[]]$snippets = Get-Content -Path "~/clipboard/snippets.txt"
-foreach ($snippet in $snippets) {
-  Write-Host $snippet
-  "$snippet" | clip.exe
-  Start-Sleep -Milliseconds 500
+function Program {
+    [CmdletBinding()]
+    param ()
+  [string[]]$snippets = Get-Content -Path "~/clipboard/snippets.txt"
+  foreach ($snippet in $snippets) {
+    Write-Host $snippet
+    "$snippet" | clip.exe
+    Start-Sleep -Milliseconds 500
+  }
 }
+
+try {
+  $msg = "Starting copying"
+  Write-EventLog -LogName 'Copy_Snippets' -Source 'Copy_Snippets' -EntryType Information -EventId 1 -Message $msg
+  Program -ErrorAction Stop
+  $msg = "Finished running"
+  Write-EventLog -LogName 'Copy_Snippets' -Source 'Copy_Snippets' -EntryType SuccessAudit -EventId 1 -Message $msg
+}
+catch {
+  $msg = $_.ToString()
+  Write-EventLog -LogName 'Copy_Snippets' -Source 'Copy_Snippets' -EntryType FailureAudit -EventId 1 -Message $msg
+}
+
