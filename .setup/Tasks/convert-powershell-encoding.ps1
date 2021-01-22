94c3ae65183c180072b7d4d4a374dd4012240c84
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-22 16:11:43 -0600

    Addresses merge conflicts

---
+++ .setup/Tasks/convert-powershell-encoding.ps1
@@ -0,0 +1,7 @@
+﻿@('*.ps1', '*.psd1', '*.psm1') `
+| % {
+  Get-ChildItem $_ -Recurse | ForEach-Object {
+    $content = Get-Content -Path $_
+    Set-Content -Path $_.Fullname -Value $content -Encoding UTF8 -PassThru -Force
+  }
+}
--- .setup/scripts/bootstrap.ps1
+++ .setup/scripts/bootstrap.ps1
@@ -1,4 +1,4 @@
-# run in user directory
+﻿# run in user directory
 
 # install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
