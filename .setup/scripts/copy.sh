ae76e21d15f931410b7ad0f3390a7c20f1c4a284
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-30 18:01:33 -0600

    Adds vundle download to copy script

--- .setup/scripts/copy.sh
+++ .setup/scripts/copy.sh
@@ -5,6 +5,14 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/swap
 
+# download vundle if it does not exist
+vunDir='~/.vim/bundle/Vundle.vim'
+[[ -d $DIR ]] || {
+  cd ~/.vim/bundle
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  cd ~
+}
+
 cp -a "$SCRIPTPATH/../copy_content_to_home/." ~/
 
 cp -a "$SCRIPTPATH/../.ahk" ~/
