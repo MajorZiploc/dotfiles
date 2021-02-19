656bbb86d53d410c00a7119ea0c367bc81e7828d
Manyu Lakhotia <lakemonyew@gmail.com>
2021-02-19 05:20:11 -0600

    Changes impl of make_copies from rn_selected's impl

--- .shared/home_bin/make_copies.sh
+++ .shared/home_bin/make_copies.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Default values
-directory='*'
-by="s/ / /"
 verbose=false
 preview=false
 
 [[ $# == 0 ]] && {
-  echo "rn_selected called with no arguments!"
-  echo "rn_selected must be called with at least 1 argument"
-  echo "use 'rn_selected -h' to see possible arguments"
+  echo "make_copies called with no arguments!"
+  echo "make_copies must be called with at least 1 argument"
+  echo "use 'make_copies -h' to see possible arguments"
   exit 1
 }
 
@@ -17,18 +15,19 @@ while [[ $# > 0 ]]
 do
   case "$1" in
 
-    -d|--directory)
-      directory="$2"
+    -f|--file)
+      og_file="$2"
       shift
       ;;
 
-    -b|--by)
-      by="$2"
+    -n|--new_file_names)
+      new_file_names="$2"
       shift
       ;;
 
-    -a|--all)
-      directory='* .[^.]*'
+    -l|--copy_location)
+      new_file_names="$2"
+      shift
       ;;
 
     -v|--verbose)
@@ -41,33 +40,38 @@ do
       ;;
 
     -h|--help)
-      echo "Command rn_selected (rename_selected):"
-      echo "Utility for renaming folders and files"
+      echo "Command make_copies:"
+      echo "Utility for making copies of a file given a list of new file names"
       echo "Usage:"
 
-      echo "    -d|--directory
-      The directory to rename folders and files in. Non-recursive. Default is: '$directory'
-      For all nonhidden files/folders in current directory non-recursive, use -d '*'
-      For all hidden files/folders in current directory non-recursive, use -d '.[^.]*'
-      For all hidden and nonhidden files/folders in current directory non-recursive, use -d '* .[^.]*'
+      echo "    -f|--file
+      Required
+      The file to make copies of.
+      Must be a file
       "
 
-      echo "    -b|--by
-      Program executed by 'sed -E' on each file name to derive new file name. Default is: $by.
-      You can do multiple s/// by doing: --by 's///;s///;s///;'
+      echo "    -n|--new_file_names
+      Required
+      The file containing the new names of the for the file.
+      Only include the name, not the path.
+      File names can have spaces.
+      Each line will be used as a file name
+      Must be a file
       "
 
-      echo "    -a|--all
-      Short hand to iterate over all hidden and non hidden files in the directory non recursively.
-      The same as specifying -d '* .[^.]*'
+      echo "    -l|--copy_location
+      Optional
+      The file location to store the copies.
+      Defaults to location of original file.
+      Must be a file path
       "
 
       echo "    -v|--verbose
-      Display the renames happening.
+      Display the copies happening.
       "
 
       echo "    -p|--preview
-      When specified, it does not preform the renames.
+      When specified, it does not preform the copies.
       When specified, it also toggles -v on.
       For debugging and 'previewing' the change before performing it.
       "
@@ -78,7 +82,7 @@ do
   
     *)
       echo "command called with non valid flags!"
-      echo "use 'rn_selected -h' to see options"
+      echo "use 'make_copies -h' to see options"
       exit 1
       ;;
 
@@ -86,30 +90,22 @@ do
   shift
 done
 
+# set copy_location
+[[ -z "$copy_location" ]] && {
+  # set copy_location based on og file path
+  copy_location=${file_path%/*}
+} || {
+  # set copy_location based on optional copy_location param
+  copy_location=$(readlink -f "$copy_location")
+}
+
 [[ $verbose == true ]] && {
-  echo "directory: $directory"
-  echo "by: $by"
+  echo "file: $og_file"
+  echo "new_file_names: $new_file_names"
+  echo "copy_location: $copy_location"
   echo "verbose: $verbose"
   echo "preview: $preview"
 }
-
-for f in $directory
-do
-  new_name=$(echo $f | sed -E "$by")
-  [[ $f == $new_name ]] && {
-    [[ $verbose == true ]] && {
-      echo "No name change for file/folder $f";
-      }
-    }
-  [[ $f != $new_name ]] && {
-    [[ $verbose == true ]] && {
-      echo "mv \"$f\" \"$new_name\"";
-      }
-    [[ $preview == false ]] && {
-      mv "$f" "$new_name";
-      }
-    }
-done
 
 
 function make_copies {
@@ -117,19 +113,15 @@ function make_copies {
   # required file
   local og_file="$1"
   # required file
-  # local new_file_names=$(cat "$2")
+  local new_file_names="$2"
   # optional folder path
   local copy_location="$3"
 
+  # location of the original file
   local file_path=$(readlink -f $og_file)
-  # set copy_location based on if it is given or not
-  [[ -z "$copy_location" ]] && {
-    copy_location=${file_path%/*}
-  } || {
-    copy_location=$(readlink -f "$copy_location")
-  }
   # ensures that copy_location exists
   mkdir -p "$copy_location"
+  # get base file name of the og file
   local base_og_file=$(basename $file_path)
 
   # while loop allows for files with spaces in name
@@ -138,7 +130,16 @@ function make_copies {
   do
     local new_file_name="$p"
     local new_file_path="$copy_location/$new_file_name-$base_og_file";
-    cp "$file_path" "$new_file_path"
-  done < "$2"
+    [[ $verbose == true ]] && {
+      echo "cp \"$file_path\" \"$new_file_path\"";
+      echo "cp \"$f\" \"$new_name\"";
+      }
+    # perform the copy only if preview is false
+    [[ $preview == false ]] && {
+      cp "$file_path" "$new_file_path"
+      }
+  done < "$new_file_names"
 }
+
+make_copies "$file" "$new_file_names" "$copy_location"
 
