82159f73f848927b8d2b5190903ea8f3b74a4c2b
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-02-24 16:14:17 -0600

    Converts utf16 to utf8

---
+++ setup/windows/Tasks/download_git_projs.ps1
@@ -0,0 +1,16 @@
+﻿
+$records = Import-Csv -Path "$PSScriptRoot/projs/github_repo_clone.csv"
+# Write-Host $records
+$root_path="C:\projects"
+cd "$root_path"
+$records `
+| % {
+  if (! (Test-Path -Path "$root_path\$($_.repo_name -replace " ", "-")")) {
+    # Write-Host "Cloning the Project $($_.repo_name)"
+    Invoke-Expression -Command $_.clone
+  }
+  else {
+    # Write-Host "Project $($_.repo_name) already exists! Not cloning this repo."
+  }
+}
+
