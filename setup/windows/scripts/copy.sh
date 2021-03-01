fbdf101ecab7e481068ee1127bc68e85467624ad
Manyu Lakhotia <lakemonyew@gmail.com>
2021-03-01 16:22:40 -0600

    Moves more res to shared

--- setup/windows/scripts/copy.sh
+++ setup/windows/scripts/copy.sh
@@ -5,7 +5,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/swap
 mkdir -p ~/vimfiles/plugin-settings
+mkdir -p ~/clipboard
 mkdir -p ~/bin
+mkdir -p /c/Tasks
+mkdir -p /c/vscodevim
 
 # download vundle if it does not exist
 vunDir='~/.vim/bundle/Vundle.vim'
@@ -16,15 +19,9 @@ vunDir='~/.vim/bundle/Vundle.vim'
 }
 
 cp -a "$SCRIPTPATH/../../../shared/required/." ~/
-
 cp -a "$SCRIPTPATH/../home/." ~/
-
 cp -a "$SCRIPTPATH/../.ahk" ~/
-
-cp -a "$SCRIPTPATH/../clipboard" ~/
-
+cp -a "$SCRIPTPATH/../../../shared/clipboard/" ~/clipboard/
 cp -a "$SCRIPTPATH/../../../shared/home_bin/." ~/bin/
-
-cp -a "$SCRIPTPATH/../Tasks/." /c/Tasks/
-
-cp -a "$SCRIPTPATH/../vscodevim/." /c/vscodevim/
+cp -a "$SCRIPTPATH/../../../shared/Tasks/." /c/Tasks/
+cp -a "$SCRIPTPATH/../../../shared/vscodevim/." /c/vscodevim/
