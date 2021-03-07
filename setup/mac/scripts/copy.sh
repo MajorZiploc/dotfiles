85f208d24c3471b86acecdd8f0773b4a6ea16fcf
Samuel Holmes <samuelholmes30@gmail.com>
2021-03-07 10:33:42 -0600

    Removes dos2unix conversion in copy script

--- setup/mac/scripts/copy.sh
+++ setup/mac/scripts/copy.sh
@@ -14,29 +14,23 @@ mkdir -p ~/vscodevim
 cd ~
 cp -a "$SCRIPTPATH/../../../shared/required/home/." .
 cp -a "$SCRIPTPATH/../home/." .
-ls -A | xargs dos2unix
 
 cd ~/bin
 cp -a "$SCRIPTPATH/../../.shared/required/home_bin/." .
-ls -A | xargs dos2unix
 
 cd ~/clipboard
 cp -a "$SCRIPTPATH/../../../shared/required/clipboard/." ~/clipboard/
-ls -A | xargs dos2unix
 
 cd ~/Tasks
 cp -a "$SCRIPTPATH/../../../shared/required/Tasks/." ~/Tasks/
-ls -A | xargs dos2unix
 
 cd ~/vscodevim
 cp -a "$SCRIPTPATH/../../../shared/required/vscodevim/." ~/vscodevim/
-ls -A | xargs dos2unix
 
 # download vundle if it does not exist
 vunDir="$HOME/.vim/bundle/Vundle.vim"
 [[ -d $vunDir ]] || {
   cd ~/.vim/bundle
   git clone https://github.com/VundleVim/Vundle.vim.git "$vunDir"
-  find . -exec readlink -f {} \; | xargs dos2unix
   cd ~
 }
