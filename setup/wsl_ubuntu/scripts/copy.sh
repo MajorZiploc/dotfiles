b3ab2015f7ead10429e7b99119b18cad2365a8e1
Manyu Lakhotia <lakemonyew@gmail.com>
2021-03-07 15:31:59 -0600

    Fixes bash.exe -> bash in ubuntu

--- setup/wsl_ubuntu/scripts/copy.sh
+++ setup/wsl_ubuntu/scripts/copy.sh
@@ -33,20 +33,22 @@ cp -a "$SCRIPTPATH/../../../shared/required/vscodevim/." ~/vscodevim/
 ls -A | xargs dos2unix
 
 # temp _vimrcterm
-cp "$SCRIPTPATH/../../../required/home/_vimrcterm" "$SCRIPTPATH/_vimrcterm"
+cp "$SCRIPTPATH/../../../shared/required/home/_vimrcterm" "$SCRIPTPATH/_vimrcterm"
 # replace .exe with nothing
-sed -i 's/bash\.exe/bash/g' "$SCRIPTPATH/_vimrcterm"
+sed -i.bak 's/bash\.exe/bash/g' "$SCRIPTPATH/_vimrcterm"
 # override _vimrcterm
 cp -a "$SCRIPTPATH/_vimrcterm" ~/_vimrcterm
 rm "$SCRIPTPATH/_vimrcterm"
+rm "$SCRIPTPATH/_vimrcterm.bak"
 
 # temp _vsvimrc 
-cp "$SCRIPTPATH/../../../required/vscodevim/_vsvimrc" "$SCRIPTPATH/_vsvimrc"
+cp "$SCRIPTPATH/../../../shared/required/vscodevim/_vsvimrc" "$SCRIPTPATH/_vsvimrc"
 # replace .exe with nothing
-sed -i 's/bash\.exe/bash/g' "$SCRIPTPATH/_vsvimrc"
+sed -i.bak 's/bash\.exe/bash/g' "$SCRIPTPATH/_vsvimrc"
 # override _vsvimrc 
 cp -a  "$SCRIPTPATH/_vsvimrc" ~/_vsvimrc
 rm "$SCRIPTPATH/_vsvimrc"
+rm "$SCRIPTPATH/_vsvimrc.bak"
 
 # download vundle if it does not exist
 vunDir="$HOME/.vim/bundle/Vundle.vim"
