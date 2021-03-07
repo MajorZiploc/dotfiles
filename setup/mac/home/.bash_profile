08db473f25aad33218b1c2bdbeb304b570284b66
Manyu Lakhotia <lakemonyew@gmail.com>
2021-03-07 10:06:26 -0600

    Inits mac settings

---
+++ setup/mac/home/.bash_profile
@@ -0,0 +1,15 @@
+# include .bashrc if it exists
+if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
---
+++ setup/mac/scripts/copy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/swap
+mkdir -p ~/vimfiles/plugin-settings
+mkdir -p ~/bin
+mkdir -p ~/clipboard
+mkdir -p ~/bin
+mkdir -p ~/Tasks
+mkdir -p ~/vscodevim
+
+cd ~
+cp -a "$SCRIPTPATH/../../../shared/required/home/." .
+cp -a "$SCRIPTPATH/../home/." .
+ls -A | xargs dos2unix
+
+cd ~/bin
+cp -a "$SCRIPTPATH/../../.shared/required/home_bin/." .
+ls -A | xargs dos2unix
+
+cd ~/clipboard
+cp -a "$SCRIPTPATH/../../../shared/required/clipboard/." ~/clipboard/
+ls -A | xargs dos2unix
+
+cd ~/Tasks
+cp -a "$SCRIPTPATH/../../../shared/required/Tasks/." ~/Tasks/
+ls -A | xargs dos2unix
+
+cd ~/vscodevim
+cp -a "$SCRIPTPATH/../../../shared/required/vscodevim/." ~/vscodevim/
+ls -A | xargs dos2unix
+
+# download vundle if it does not exist
+vunDir="$HOME/.vim/bundle/Vundle.vim"
+[[ -d $vunDir ]] || {
+  cd ~/.vim/bundle
+  git clone https://github.com/VundleVim/Vundle.vim.git "$vunDir"
+  find . -exec readlink -f {} \; | xargs dos2unix
+  cd ~
+}
