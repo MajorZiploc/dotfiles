ebd4facee1b62146cfefd3a77346643f88a6b59e
Manyu Lakhotia <lakemonyew@gmail.com>
2021-03-08 05:51:25 -0600

    Converts format for files in this repo only ubuntu

--- setup/wsl_ubuntu/scripts/copy.sh
+++ setup/wsl_ubuntu/scripts/copy.sh
@@ -2,6 +2,9 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+find "$SCRIPTPATH/../../../shared/" -type f -exec dos2unix {} \;
+find "$SCRIPTPATH/../" -type f -exec dos2unix {} \;
+
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/swap
 mkdir -p ~/vimfiles/plugin-settings
@@ -11,26 +14,12 @@ mkdir -p ~/bin
 mkdir -p ~/Tasks
 mkdir -p ~/vscodevim
 
-cd ~
-cp -a "$SCRIPTPATH/../../../shared/required/home/." .
-cp -a "$SCRIPTPATH/../home/." .
-ls -A | xargs dos2unix
-
-cd ~/bin
-cp -a "$SCRIPTPATH/../../../shared/required/home_bin/." .
-ls -A | xargs dos2unix
-
-cd ~/clipboard
+cp -a "$SCRIPTPATH/../../../shared/required/home/." ~/
+cp -a "$SCRIPTPATH/../home/." ~/
+cp -a "$SCRIPTPATH/../../../shared/required/home_bin/." ~/
 cp -a "$SCRIPTPATH/../../../shared/required/clipboard/." ~/clipboard/
-ls -A | xargs dos2unix
-
-cd ~/Tasks
 cp -a "$SCRIPTPATH/../../../shared/required/Tasks/." ~/Tasks/
-ls -A | xargs dos2unix
-
-cd ~/vscodevim
 cp -a "$SCRIPTPATH/../../../shared/required/vscodevim/." ~/vscodevim/
-ls -A | xargs dos2unix
 
 # temp _vimrcterm
 cp "$SCRIPTPATH/../../../shared/required/home/_vimrcterm" "$SCRIPTPATH/_vimrcterm"
@@ -55,6 +44,10 @@ vunDir="$HOME/.vim/bundle/Vundle.vim"
 [[ -d $vunDir ]] || {
   cd ~/.vim/bundle
   git clone https://github.com/VundleVim/Vundle.vim.git "$vunDir"
-  find . -exec readlink -f {} \; | xargs dos2unix
+  find ~/.vim/bundle -type f -exec dos2unix {} \;
   cd ~
 }
+
+find "$SCRIPTPATH/../../../shared/" -type f -exec unix2dos {} \;
+find "$SCRIPTPATH/../" -type f -exec unix2dos {} \;
+
