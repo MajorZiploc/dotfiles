b6aa4ac930ae17e514d3199e02021ae74ace28eb
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-03-09 09:08:33 -0600

    Creates edit file script called by edit styles

---
+++ shared/copy_scripts/edit_files.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function edit_files {
+  local temp="$1"
+  local tempShared="$2"
+  local tempThis="$3"
+  local edit_style="$4"
+
+  shopt -s globstar
+  shopt -s dotglob
+  shopt -s nocasematch
+  
+  edit_files_helper "$tempThis/$edit_style/home/*" "$tempShared/required/home" "$edit_style"
+  edit_files_helper "$tempThis/$edit_style/Tasks/*" "$tempShared/required/Tasks" "$edit_style"
+  edit_files_helper "$tempThis/$edit_style/clipboard/*" "$tempShared/required/clipboard" "$edit_style"
+  edit_files_helper "$tempThis/$edit_style/vscodevim/*" "$tempShared/required/vscodevim" "$edit_style"
+}
+
+function edit_files_helper {
+  local dirGlob="$1"
+  local destDir="$2"
+  local estyle="$3"
+  for i in $dirGlob ; do
+    [[ -f "$i" ]] && {
+      local b=$(basename "$i");
+      [[ $estyle == "append" ]] && {
+        echo "cat \"$i\" >> \"$destDir/$b"
+        echo "rm \"$i\""
+        #cat "$i" >> "$destDir/$b"
+        #rm "$i"
+      }
+      [[ $estyle == "prepend" ]] && {
+        echo "cat <(cat \"$i\") <(cat \"$destDir/$b\") > \"$destDir/$b\""
+        echo "rm \"$i\""
+        #cat <(cat "$i") <(cat "$destDir/$b") >> "$destDir/$b"
+        #rm "$i"
+      }
+      [[ $estyle == "override" ]] && {
+        echo "cat \"$i\" > \"$destDir/$b\""
+        echo "rm \"$i\""
+        #cat "$i" > "$destDir/$b"
+        #rm "$i"
+      }
+    }
+  done;
+}
+
+edit_files "$1" "$2" "$3" "$4"
+
