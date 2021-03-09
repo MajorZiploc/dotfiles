19c378c3a7e7bf0b9e3f90fb507c732badfd9628
Manyu Lakhotia <lakemonyew@gmail.com>
2021-03-09 07:45:44 -0600

    Inits append script

--- shared/copy_scripts/append.sh
+++ shared/copy_scripts/append.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+function append {
+  local temp="$1"
+  local tempShared="$2"
+  local tempThis="$3"
+
+  shopt -s globstar
+  shopt -s dotglob
+  
+  local directory="$tempThis/append/home/*"
+  for f in $directory ; do
+    [[ -f "$f" ]] && {
+      echo "$f";
+      local b=$(basename "$f")
+      echo "$b"
+      # cat "$f" >> "$tempShared/required/home/$b"
+      # rm "$f"
+    }
+  done;
+}
+
+append 1 2 "$1"
+
