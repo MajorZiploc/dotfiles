4dca5762d109b7f4ac3fe457a06ef1ea926be178
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-03-09 10:14:02 -0600

    Reworks win copy based on mac copy

--- setup/windows/scripts/copy.sh
+++ setup/windows/scripts/copy.sh
@@ -2,6 +2,15 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+temp="$SCRIPTPATH/../../temp"
+tempShared="$temp/shared"
+tempThis="$SCRIPTPATH/../../temp/this"
+
+mkdir -p "$tempShared/"
+cp -r "$SCRIPTPATH/../../../shared/" "$temp/"
+mkdir -p "$tempThis/"
+cp -r "$SCRIPTPATH/../" "$tempThis/"
+
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/swap
 mkdir -p ~/vimfiles/plugin-settings
@@ -9,6 +18,10 @@ mkdir -p ~/clipboard
 mkdir -p ~/bin
 mkdir -p /c/Tasks
 mkdir -p /c/vscodevim
+
+$tempShared/copy_scripts/edit_files.sh "$temp" "$tempShared" "$tempThis" "append"
+$tempShared/copy_scripts/edit_files.sh "$temp" "$tempShared" "$tempThis" "prepend"
+$tempShared/copy_scripts/edit_files.sh "$temp" "$tempShared" "$tempThis" "override"
 
 # download vundle if it does not exist
 vunDir="$HOME/.vim/bundle/Vundle.vim"
@@ -18,10 +31,17 @@ vunDir="$HOME/.vim/bundle/Vundle.vim"
   cd ~
 }
 
-cp -a "$SCRIPTPATH/../../../shared/required/home/." ~/
-cp -a "$SCRIPTPATH/../home/." ~/
 cp -a "$SCRIPTPATH/../.ahk" ~/
-cp -a "$SCRIPTPATH/../../../shared/required/clipboard/." ~/clipboard/
-cp -a "$SCRIPTPATH/../../../shared/required/home_bin/." ~/bin/
-cp -a "$SCRIPTPATH/../../../shared/required/Tasks/." /c/Tasks/
-cp -a "$SCRIPTPATH/../../../shared/required/vscodevim/." /c/vscodevim/
+
+cp -a "$tempShared/required/home/." ~/
+cp -a "$tempShared/required/home_bin/." ~/bin/
+cp -a "$tempShared/required/clipboard/." ~/clipboard/
+cp -a "$tempShared/required/Tasks/." /c/Tasks/
+cp -a "$tempShared/required/vscodevim/." /c/vscodevim/
+
+rm -r "$temp/"
+
+unset tempShared
+unset tempThis
+unset temp
+
