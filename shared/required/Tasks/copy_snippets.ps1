84ef48523ad814382c500be681551a8df611c281
Manyu Lakhotia <lakemonyew@gmail.com>
2021-04-18 10:18:15 -0500

    Refactors copy_snippets

--- shared/required/Tasks/copy_snippets.ps1
+++ shared/required/Tasks/copy_snippets.ps1
@@ -1,39 +1,49 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory=$false)]
-    [ValidateSet("bash_oneline", "sql_block", "all")]
-    [string]
-    $type = "all"
+  [Parameter(Mandatory = $false)]
+  [ValidateSet("bash", "sql", "all")]
+  [string]
+  $type = "all"
 )
 
 $choices = @{
-  'bash_oneline' = @{
-    'snippets_file' = "~/clipboard/bash/oneline.txt"
-    'delimiter'= "`n"
+  'bash' = @{
+    'folder' = "~/clipboard/bash/"
+    'files' = @{
+      'oneline' = @{
+        'delimiter'     = "`n"
+      }
+    }
   }
-  'sql_block' = @{
-    'snippets_file' = "~/clipboard/sql/block.txt"
-    'delimiter'= "<:>"
+  'sql'  = @{
+    'folder' = "~/clipboard/sql/"
+    'files' = @{
+      'block' = @{
+        'delimiter'     = "<:>"
+      }
+    }
   }
 }
 
 function Copy-Snippets {
   [CmdletBinding()]
   param ()
-  $snippets_file = $choice['snippets_file']
-  $delimiter = $choice['delimiter']
-  Write-Host "`n"
-  Write-Host "------------------------"
-  Write-Host "type: $key"
-  Write-Host "snippets_file: $snippets_file"
-  Write-Host "delimiter: $delimiter"
-  Write-Host "------------------------"
-  [string[]]$snippets = Get-Content -Path $snippets_file -Delimiter $delimiter
-  foreach ($snippet in $snippets) {
-    if (![string]::IsNullOrWhiteSpace("$snippet")) {
-      Write-Host "$snippet"
-      "$snippet" | clip.exe
-      Start-Sleep -Milliseconds 500
+  $choice['files'].Keys | ForEach-Object {
+    $snippets_file = $choice['folder'] + $_ + '.txt'
+    $delimiter = $choice['files'][$_]['delimiter']
+    Write-Host "`n"
+    Write-Host "------------------------"
+    Write-Host "type: $key"
+    Write-Host "snippets_file: $snippets_file"
+    Write-Host "delimiter: $delimiter"
+    Write-Host "------------------------"
+    [string[]]$snippets = Get-Content -Path $snippets_file -Delimiter $delimiter
+    foreach ($snippet in $snippets) {
+      if (![string]::IsNullOrWhiteSpace("$snippet")) {
+        Write-Host "$snippet"
+        "$snippet" | clip.exe
+        Start-Sleep -Milliseconds 500
+      }
     }
   }
 }
