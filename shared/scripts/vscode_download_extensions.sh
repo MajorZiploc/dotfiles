22b878d1fc46ae1d43757bcf958ca5c33cfb613b
Manyu Lakhotia <lakemonyew@gmail.com>
2021-04-28 19:30:43 -0500

    Adds vscode ext copy script

---
+++ shared/scripts/vscode_download_extensions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# General plugings
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension janisdd.vscode-edit-csv
+code --install-extension mechatroner.rainbow-csv
+code --install-extension mhutchie.git-graph
+code --install-extension ms-vscode.powershell
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension vscodevim.vim
+
+# dotnet
+code --install-extension Ionide.Ionide-fsharp
+code --install-extension ms-dotnettools.csharp
+
+# Python
+code --install-extension almenon.arepl
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
+
+# bash wsl
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension rogalmic.bash-debug
