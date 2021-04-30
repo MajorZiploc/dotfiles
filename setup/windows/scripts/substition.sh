9d21aef7bb1b523ebb191b7c67142ebe7d3ea887
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-04-30 09:11:15 -0500

    Fixes windows copy sub fn

--- setup/windows/scripts/substition.sh
+++ setup/windows/scripts/substition.sh
@@ -8,8 +8,7 @@ temp="$2"
 tempShared="$3"
 tempThis="$4"
 
-find . -maxdepth 9 -regextype egrep -iregex ".*$tempShared/scripts.*" -type f -exec sed -i.bak 's,\$HOME/(Tasks|vscodevim),/c/\1,g' {} \;
-
+find "$tempShared/scripts"  -maxdepth 9 -regextype egrep -iregex ".*" -type f -exec sed -Ei.bak 's,\$HOME/(Tasks|vscodevim),/c/\1,g' {} \;
 find "$tempShared" -regextype egrep -iregex '.*\.bak$' -type f -exec rm {} \;
 
 unset setupRoot
