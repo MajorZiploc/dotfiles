c5b13ec84ca69021666299424739f7ea620552fc
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-02 15:11:41 -0500

    Makes all sed subs global

--- setup/mac/scripts/substition.sh
+++ setup/mac/scripts/substition.sh
@@ -11,12 +11,12 @@ tempThis="$4"
 find "$tempShared/" -type f -exec dos2unix {} \;
 find "$tempThis/" -type f -exec dos2unix {} \;
 
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's/VIM_PLUGIN_IMPORT_PLACEHOLDER/so ~\/_vim_plugins/' {} \;
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's/VSC_INTEGRATED_SHELL_PLACEHOLDER/\/bin\/bash/' {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's/VIM_PLUGIN_IMPORT_PLACEHOLDER/so ~\/_vim_plugins/g' {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's/VSC_INTEGRATED_SHELL_PLACEHOLDER/\/bin\/bash/g' {} \;
 vsvimpath="$(echo "$HOME/vscodevim/_vsvimrc")"
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,VSVIM_DIR_PLACEHOLDER,$vsvimpath," {} \;
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,OS_PLACE_HOLDER,mac," {} \;
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's,VIM_SHELL_PLACEHOLDER,/bin/bash,' {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,VSVIM_DIR_PLACEHOLDER,$vsvimpath,g" {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,OS_PLACE_HOLDER,mac,g" {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's,VIM_SHELL_PLACEHOLDER,/bin/bash,g' {} \;
 # example of deleting bak files
 # find "$tempShared" -regextype egrep -iregex '.*\.bak$' -type f -exec rm {} \;
 
--- setup/windows/scripts/substition.sh
+++ setup/windows/scripts/substition.sh
@@ -9,11 +9,11 @@ tempShared="$3"
 tempThis="$4"
 
 vsvimpath="$(echo "$HOME/vscodevim/_vsvimrc" | sed 's,^/c,c:,g' | sed -E 's,/,\\\\\\\\,g')"
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's/VIM_PLUGIN_IMPORT_PLACEHOLDER/so ~\/_vim_plugins/' {} \;
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,VSVIM_DIR_PLACEHOLDER,$vsvimpath," {} \;
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,OS_PLACE_HOLDER,windows," {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's/VIM_PLUGIN_IMPORT_PLACEHOLDER/so ~\/_vim_plugins/g' {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,VSVIM_DIR_PLACEHOLDER,$vsvimpath,g" {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,OS_PLACE_HOLDER,windows,g" {} \;
 find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's/VSC_INTEGRATED_SHELL_PLACEHOLDER/C:\\\\Program Files\\\\Git\\\\bin\\\\bash.exe/g' {} \;
-find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's,VIM_SHELL_PLACEHOLDER,/bin/bash.exe,' {} \;
+find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -i'' 's,VIM_SHELL_PLACEHOLDER,/bin/bash.exe,g' {} \;
 
 unset setupRoot
 unset tempShared
