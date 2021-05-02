ae9351a8023c309b8e1dcb6bd23f3e15daf469aa
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-02 12:22:03 -0500

    Fixes vscodevim home path config

--- setup/windows/scripts/substition.sh
+++ setup/windows/scripts/substition.sh
@@ -9,7 +9,8 @@ tempShared="$3"
 tempThis="$4"
 
 find "$tempShared" -regextype egrep -iregex '.*bash_functions.*' -type f -exec sed -E -i'' 's,(local project_root_path=)"~/projects/home-settings",\1"/c/projects/home-settings",' {} \;
-find "$tempShared" -regextype egrep -iregex ".*settings.json$" -type f -exec sed -E -i'' "s,\$HOME(\\\\_vsvimrc),/$HOME\1,g" {} \;
+vsvimpath="$(echo "$HOME" | sed 's,^/c,c:,g' | sed -E 's,/,\\\\\\\\,g')"
+find "$tempShared" -regextype egrep -iregex ".*settings.json" -type f -exec sed -E -i'' "s,HOME_DIR_PLACEHOLDER,$vsvimpath," {} \;
 
 unset setupRoot
 unset tempShared
