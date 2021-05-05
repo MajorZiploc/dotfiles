2b57c3f307fdf6dda438b5e2d8736459521cb7f3
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-05-05 16:05:54 -0500

    Switches default to github clone

--- shared/required/Tasks/git_download_projs.ps1
+++ shared/required/Tasks/git_download_projs.ps1
@@ -1,6 +1,6 @@
 ﻿
-$records = Import-Csv -Path "$PSScriptRoot/projs/bitbucket_clone.csv"
-# $records = Import-Csv -Path "$PSScriptRoot/projs/github_repos_clone.csv"
+# $records = Import-Csv -Path "$PSScriptRoot/projs/bitbucket_clone.csv"
+$records = Import-Csv -Path "$PSScriptRoot/projs/github_repos_clone.csv"
 # Write-Host $records
 $root_path="~/projects"
 cd "$root_path"
@@ -9,10 +9,10 @@ $records `
   if (! (Test-Path -Path "$root_path\$($_.repo_name -replace " ", "-")")) {
     # Write-Host "Cloning the Project $($_.repo_name)"
     # bitbucket
-    git clone $_.clone
+    # Invoke-Expression -Command $_.clone
 
     # github
-    # Invoke-Expression -Command $_.clone
+    git clone $_.clone
   }
   else {
     # Write-Host "Project $($_.repo_name) already exists! Not cloning this repo."
