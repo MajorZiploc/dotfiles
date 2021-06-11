d8839874fcbcb7da33f80ec047df1d2190a06f73
Manyu Lakhotia <lakemonyew@gmail.com>
2021-06-11 05:36:55 -0500

    Adds a find_in_files testcase

--- tests/check_find_cmds.sh
+++ tests/check_find_cmds.sh
@@ -4,3 +4,24 @@ load 'libs/bats-assert/load'
 
 source ~/.bashrc || true;
 
+@test "check find_in_files '\bdf\.Columns' for proper listing of occurrences" {
+  cd ./mock_content
+  run find_in_files '\bdf\.Columns'
+  assert_success
+expected="$(cat << EOF
+./pandas_data_analytics/src/body_comp/run.py:38:# exps = Enumerable(df.columns).where(lambda c: re.match('.*_exp',c,re.I)).to_list()
+./pandas_data_analytics/src/osrs/run.py:29:exps = Enumerable(df.columns).where(lambda c: re.match('.*_exp', c, re.I)).to_list()
+./pandas_data_analytics/src/osrs/run.py:94:exp_df: pd.DataFrame = df.loc[:, df.columns.isin(Enumerable(
+./pandas_data_analytics/src/osrs/run.py:95:  df.columns).where(lambda c: re.match('.*exp', c)).to_list())]
+./pandas_data_analytics/src/common_food/run.py:19:df.columns = df.columns.str.lower()
+./pandas_data_analytics/src/utils/utils.py:6:  return [df.head(), df.dtypes, df.shape, df.columns, df.index]
+./pandas_data_analytics/src/pokemon_learn_sets/run.py:149:  # print(df.columns)
+./FslabDataAnalytics/FslabDataAnalytics/osrs/run.fs:30:        df.Columns.Keys
+./FslabDataAnalytics/FslabDataAnalytics/osrs/run.fs:39:    printfn "%A" <| df.Columns.Keys
+EOF
+)"
+  assert_output "$expected"
+  cd ..
+}
+
+
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,4 +2,5 @@
 
 ./check_bash_sourcing.sh
 ./check_for_special_bash_fns.sh
+./check_find_cmds.sh
 
