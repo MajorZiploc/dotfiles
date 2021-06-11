b5bd94b78268375a6516066e9ceaa2a01064e907
Manyu Lakhotia <lakemonyew@gmail.com>
2021-06-11 15:15:30 -0500

    Adds find_files_fuzz test case

--- tests/check_find_files.sh
+++ tests/check_find_files.sh
@@ -17,3 +17,22 @@ EOF
   cd ..
 }
 
+@test "check find_files_fuzz '.*pa.*' '' '2' for proper listing of occurrences with a file pattern filter and a max depth search of 2" {
+  cd ./mock_content
+  run find_files_fuzz '.*pa.*' '' '2'
+  assert_success
+expected="$(cat << EOF
+./prolog_prac/hello_world.pl
+./prolog_prac/README.md
+./pandas_data_analytics/Dockerfile
+./pandas_data_analytics/Pipfile
+./pandas_data_analytics/docker-compose.yaml
+./pandas_data_analytics/pyproject.toml
+./pandas_data_analytics/setup.py
+./pandas_data_analytics/README.md
+EOF
+)"
+  assert_output "$expected"
+  cd ..
+}
+
