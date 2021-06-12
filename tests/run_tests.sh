cc2034d6a65afff8b91f22081831817cc9440c58
Manyu Lakhotia <lakemonyew@gmail.com>
2021-06-12 08:07:15 -0500

    Performs placeholder test first

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 tests=(
+"check_placeholders.sh"
 "check_bash_sourcing.sh"
 "check_for_special_bash_fns.sh"
 "check_find_in_files.sh"
 "check_find_files.sh"
-"check_placeholders.sh"
 )
 
 for test in ${tests[@]};
