a0a2662e99f43ff7c175e05fe99fc1e0af474dea
Manyu Lakhotia <lakemonyew@gmail.com>
2021-06-12 09:30:57 -0500

    Adds vim placeholder text tests

--- tests/check_placeholders.sh
+++ tests/check_placeholders.sh
@@ -4,7 +4,7 @@ load 'libs/bats-assert/load'
 
 source ~/.bashrc || true;
 
-@test "check home content to make sure _PLACEHOLDER doesnt exist in the files" {
+@test "check bash content to make sure _PLACEHOLDER doesnt exist in the files" {
 files=(
   "$HOME/.bash_profile"
   "$HOME/.bashrc"
@@ -13,11 +13,38 @@ files=(
 )
 for file in ${files[@]};
   do
-    
+
     run grep "_PLACEHOLDER" "$file";
     assert_failure
     assert_output ""
   done;
 
+}
+
+@test "check home vim content to make sure _PLACEHOLDER doesnt exist in the files" {
+  files=(
+  "$HOME/_commonvimrc"
+  "$HOME/_vimrcterm"
+  "$HOME/_vim_plugins"
+  "$HOME/.vimrc"
+  )
+  for file in ${files[@]};
+    do
+
+      run grep "_PLACEHOLDER" "$file";
+      assert_failure
+      assert_output ""
+    done;
+}
+
+@test "check vim plugin config content to make sure _PLACEHOLDER doesnt exist in the files" {
+  files=`find ~/vimfiles/plugin-settings -maxdepth 1 -type f`;
+  for file in ${files[@]};
+    do
+
+      run grep "_PLACEHOLDER" "$file";
+      assert_failure
+      assert_output ""
+    done;
 }
 
