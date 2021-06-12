5ddf77059ff45a13a5e2cad4481d80f47db03d31
Manyu Lakhotia <lakemonyew@gmail.com>
2021-06-12 05:46:10 -0500

    Reformats test running

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-./check_bash_sourcing.sh
-./check_for_special_bash_fns.sh
-./check_find_in_files.sh
+tests=(
+"check_bash_sourcing.sh"
+"check_for_special_bash_fns.sh"
+"check_find_in_files.sh"
+"check_find_files.sh"
+)
+
+for test in ${tests[@]};
+  do
+    echo "---------------"
+    echo "Running $test"
+    echo ""
+    ./"$test";
+    echo "---------------"
+    echo ""
+done;
 
