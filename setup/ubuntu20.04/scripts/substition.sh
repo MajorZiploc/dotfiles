ee8e1f42708db3352dc4d150d68b8ddd416431d2
Manyu Lakhotia <lakemonyew@gmail.com>
2021-06-21 17:46:19 -0500

    Combines vim settings/include placeholders into 1 each

--- setup/ubuntu20.04/scripts/substition.sh
+++ setup/ubuntu20.04/scripts/substition.sh
@@ -12,22 +12,10 @@ vsvimpath="$(echo "$HOME/vscodevim/_vsvimrc")"
 find "$tempShared" -regextype egrep -iregex ".*\.json" -type f -exec sed -E -i'' "s,VSVIM_DIR_PLACEHOLDER,$vsvimpath,g" {} \;
 find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' 's/VIM_PLUGIN_IMPORT_PLACEHOLDER/so ~\/_vim_plugins/g' {} \;
 find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' 's,VIM_SHELL_PLACEHOLDER,/bin/bash,g' {} \;
-vim_plugin_import_ctrlp="Plugin 'ctrlpvim/ctrlp.vim' \" fuzzy file finder";
-find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_IMPORT_CTRLP_PLACEHOLDER,$vim_plugin_import_ctrlp,g" {} \;
-vim_plugin_import_rip_grep="Plugin 'jremmen/vim-ripgrep' \" grepper";
-find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_IMPORT_RIP_GREP_PLACEHOLDER,$vim_plugin_import_rip_grep,g" {} \;
-vim_plugin_import_quickfix="Plugin 'stefandtw/quickfix-reflector.vim' \" editable quickfix list for ripgrep";
-find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_IMPORT_QUICKFIX_PLACEHOLDER,$vim_plugin_import_quickfix,g" {} \;
-vim_plugin_import_fzf="\"Plugin 'junegunn/fzf'\, { 'do': { -> fzf#install() } }\nPlugin 'junegunn/fzf.vim'\nPlugin 'airblade/vim-rooter'\n";
-find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_IMPORT_FZF_PLACEHOLDER,$vim_plugin_import_fzf,g" {} \;
-vim_plugin_settings_ctrlp="so ~/vimfiles/plugin-settings/ctrlp.vim";
-find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_SETTINGS_CTRLP_PLACEHOLDER,$vim_plugin_settings_ctrlp,g" {} \;
-vim_plugin_settings_rip_grep="so ~/vimfiles/plugin-settings/ripgrep.vim";
-find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_SETTINGS_RIP_GREP_PLACEHOLDER,$vim_plugin_settings_rip_grep,g" {} \;
-vim_plugin_settings_quickfix="so ~/vimfiles/plugin-settings/quickfix-reflector.vim";
-find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_SETTINGS_QUICKFIX_PLACEHOLDER,$vim_plugin_settings_quickfix,g" {} \;
-vim_plugin_settings_fzf="\"so ~/vimfiles/plugin-settings/fzf.vim";
-find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_SETTINGS_FZF_PLACEHOLDER,$vim_plugin_settings_fzf,g" {} \;
+vim_plugin_include="Plugin 'ctrlpvim/ctrlp.vim' \" fuzzy file finder\nPlugin 'jremmen/vim-ripgrep' \" grepper\nPlugin 'stefandtw/quickfix-reflector.vim' \" editable quickfix list for ripgrep\n\"Plugin 'junegunn/fzf'\, { 'do': { -> fzf#install() } }\n\"Plugin 'junegunn/fzf.vim'\nPlugin 'airblade/vim-rooter'";
+find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_INCLUDE_PLACEHOLDER,$vim_plugin_include,g" {} \;
+vim_plugin_settings="so ~/vimfiles/plugin-settings/ctrlp.vim\nso ~/vimfiles/plugin-settings/ripgrep.vim\nso ~/vimfiles/plugin-settings/quickfix-reflector.vim\n\"so ~/vimfiles/plugin-settings/fzf.vim";
+find "$tempShared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_SETTINGS_PLACEHOLDER,$vim_plugin_settings,g" {} \;
 find "$tempShared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,OS_PLACEHOLDER,ubuntu20.04,g" {} \;
 vsc_settings_destination_placeholder="\$HOME/.config/Code/User/";
 find "$tempShared" -regextype egrep -iregex ".*\.sh" -type f -exec sed -E -i'' "s,VSC_SETTINGS_DESTINATION_PLACEHOLDER,$vsc_settings_destination_placeholder,g" {} \;
@@ -37,14 +25,8 @@ fuzzy_finder_placeholder="fzf";
 find "$tempShared" -regextype egrep -iregex ".*bash.*" -type f -exec sed -E -i'' "s,FUZZY_FINDER_PLACEHOLDER,$fuzzy_finder_placeholder,g" {} \;
 
 unset vsvimpath
-unset vim_plugin_import_ctrlp
-unset vim_plugin_import_rip_grep
-unset vim_plugin_import_quickfix
-unset vim_plugin_import_fzf
-unset vim_plugin_settings_ctrlp
-unset vim_plugin_settings_rip_grep
-unset vim_plugin_settings_quickfix
-unset vim_plugin_settings_fzf
+unset vim_plugin_include
+unset vim_plugin_settings
 unset tmuxps_paths_array_placeholder
 unset fuzzy_finder_placeholder
 unset vsc_settings_destination_placeholder
