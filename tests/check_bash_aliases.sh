88175b574661861f47b15868bbaf0fcd44179b97
Manyu Lakhotia <lakemonyew@gmail.com>
2021-07-04 15:37:56 -0500

    Adds to_unixpath tests

--- tests/check_bash_aliases.sh
+++ tests/check_bash_aliases.sh
@@ -24,3 +24,27 @@ source ~/.bashrc || true;
   assert_output "$expected"
 }
 
+@test "check that to_unixpath" { function f(){
+    echo "$1" | to_unixpath;
+  }
+  path='C:\mnt\Users\someone\stuff.txt'
+  run f "$path"
+  assert_success
+  expected='/c/mnt/Users/someone/stuff.txt'
+  assert_output "$expected"
+  path='\mnt\Users\someone\stuff.txt'
+  run f "$path"
+  assert_success
+  expected='/mnt/Users/someone/stuff.txt'
+  assert_output "$expected"
+  path='J:\mnt\Users\someone\stuff.txt'
+  run f "$path"
+  assert_success
+  expected='/j/mnt/Users/someone/stuff.txt'
+  assert_output "$expected"
+  path='Jo:\mnt\Users\someone\stuff.txt'
+  run f "$path"
+  assert_success
+  expected='Jo:/mnt/Users/someone/stuff.txt'
+  assert_output "$expected"
+}
