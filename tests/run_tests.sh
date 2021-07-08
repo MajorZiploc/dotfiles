1e8a6f4dfacee99998b0cf7f963a988d04c55e34
Manyu Lakhotia <abhimanyu.lakhotia@gmail.com>
2021-07-08 16:33:22 -0500

    Adds missing tests to run all tests script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,9 +3,11 @@
 tests=(
 "check_placeholders.sh"
 "check_bash_sourcing.sh"
+"check_bash_aliases.sh"
 "check_existence.sh"
 "check_find_in_files.sh"
 "check_find_files.sh"
+"check_set_fns.sh"
 )
 
 for test in ${tests[@]};
