ce34494911ead694e1cd24cf4f515bd34d9f854c
Manyu Lakhotia <lakemonyew@gmail.com>
2021-08-18 13:36:58 -0500

    Mvs refresh_pwsh bash fn to Tasks bash script

---
+++ shared/required/Tasks/pwsh_refresh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Refreshing powershell modules...";
+echo '';
+pwsh -Command '& {@("powershell_scaffolder", "PSLogFileReporter") | ForEach-Object { Install-Module -Name "$_" -Scope CurrentUser -Force;}}';
+echo '';
+echo "Refresh completed.";
+
