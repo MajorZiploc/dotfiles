afc073af12e400f36fbe830a5be0e0aaf746443c
Manyu Lakhotia <lakemonyew@gmail.com>
2021-09-12 16:00:39 -0500

    Adds prolog configs/install

---
+++ install_prolog.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run in swipl interative, this is for coc-vim language server support
+# pack_install(lsp_server).
+
+sudo add-apt-repository ppa:swi-prolog/stable
+sudo apt-get update
+sudo apt-get install swi-prolog
+
