aed7f5123a15496d28f9a6e5d377b89c44517649
Manyu Lakhotia <abhimanyu.lakhotia@gmail.com>
2021-09-29 14:01:28 -0500

    Adds sql_exec to wsl and win10 bash functions

--- setup/windows10/append/home/.bashrc.d/functions.bash
+++ setup/windows10/append/home/.bashrc.d/functions.bash
@@ -4,3 +4,20 @@ function cmd_exec {
   [[ -z "$command" ]] && { echo "Must specify command parameter!" >&2; return 1; }
   cmd.exe "/c $command";
 }
+
+function sql_exec {
+  local server="$1";
+  local database="$2";
+  local sql_cmd="$3";
+  local column_separator_char="$4";
+  [[ -z "$server" ]] && { echo "Must specify a server!" >&2; return 1; }
+  [[ -z "$database" ]] && { echo "Must specify a database!" >&2; return 1; }
+  [[ -z "$sql_cmd" ]] && { echo "Must specify a sql_cmd file or string!" >&2; return 1; }
+  echo "${column_separator_char:="~"}" >/dev/null;
+  if [[ -f "$sql_cmd" ]]; then
+    sqlcmd.exe -S "$server" -E -i "$sql_cmd" -d "$database" -W -s "$column_separator_char";
+  else
+    sqlcmd.exe -S "$server" -E -Q "$sql_cmd" -d "$database" -W -s "$column_separator_char";
+  fi
+}
+
--- setup/wsl_ubuntu20.04/append/home/.bashrc.d/functions.bash
+++ setup/wsl_ubuntu20.04/append/home/.bashrc.d/functions.bash
@@ -1,6 +1,23 @@
 function cmd_exec {
   # executes command prompt commands or file
   local command="$1";
-  [[ -z "$command" ]] && { echo "Missing command parameter!" >&2; return 1; }
-  cmd.exe "/c $1";
+  [[ -z "$command" ]] && { echo "Must specify command parameter!" >&2; return 1; }
+  cmd.exe "/c $command";
 }
+
+function sql_exec {
+  local server="$1";
+  local database="$2";
+  local sql_cmd="$3";
+  local column_separator_char="$4";
+  [[ -z "$server" ]] && { echo "Must specify a server!" >&2; return 1; }
+  [[ -z "$database" ]] && { echo "Must specify a database!" >&2; return 1; }
+  [[ -z "$sql_cmd" ]] && { echo "Must specify a sql_cmd file or string!" >&2; return 1; }
+  echo "${column_separator_char:="~"}" >/dev/null;
+  if [[ -f "$sql_cmd" ]]; then
+    sqlcmd.exe -S "$server" -E -i "$sql_cmd" -d "$database" -W -s "$column_separator_char";
+  else
+    sqlcmd.exe -S "$server" -E -Q "$sql_cmd" -d "$database" -W -s "$column_separator_char";
+  fi
+}
+
