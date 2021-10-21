b5a11b80a39bf32b688ba9a4514b7cf7ffe9ac0b
Manyu Lakhotia <lakemonyew@gmail.com>
2021-10-20 20:34:02 -0500

    Replaces all \bsed occurrences in bash functions and aliases for mac

--- setup/mac/scripts/substition.sh
+++ setup/mac/scripts/substition.sh
@@ -32,8 +32,7 @@ find -E "$tempShared" -iregex ".*bash.*" -type f -exec gsed -E -i'' "s,EXTRA_ENV
 coc_plugins_placeholder="let g:coc_global_extensions=['coc-json', 'coc-pyright', 'coc-sql', 'coc-tsserver', 'coc-css', 'coc-html', 'coc-sh']";
 find -E "$tempShared" -iregex ".*coc.*" -type f -exec gsed -E -i'' "s/COC_PLUGINS_PLACEHOLDER/$coc_plugins_placeholder/g" {} \;
 find -E "$tempShared" -type f -exec gsed -E -i'' "s/find(.*?)-regextype egrep/find -E\1/g" {} \;
-find -E "$tempShared" -iregex ".*functions\.bash" -type f -exec gsed -E -i'' "s/-exec sed/-exec gsed/g" {} \;
-find -E "$tempShared" -iregex ".*aliases\.bash" -type f -exec gsed -E -i'' "s/\bsed /gsed /g" {} \;
+find -E "$tempShared" -iregex ".*(functions|aliases).*\.bash" -type f -exec gsed -E -i'' "s/\bsed /gsed /g" {} \;
 # example of deleting bak files
 # find "$tempShared" -iregex '.*\.bak$' -type f -exec rm {} \;
 
