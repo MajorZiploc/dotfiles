0670769e51d8091e12e3a39f25d1cd67b1ed2713
Manyu Lakhotia <lakemonyew@gmail.com>
2021-10-30 12:21:39 -0500

    Cleans up wsl ubuntu scripts

--- setup/wsl_ubuntu20.04/scripts/install.sh
+++ setup/wsl_ubuntu20.04/scripts/install.sh
@@ -54,6 +54,22 @@ sudo apt-get update
 sudo apt-get install -y dotnet-sdk-3.1
 sudo apt-get install -y dotnet-sdk-5.0
 
+# powershell
+# Install pre-requisite packages.
+sudo apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+# Update the list of products
+sudo apt-get update
+# Enable the "universe" repositories
+sudo add-apt-repository universe
+# Install PowerShell
+sudo apt-get install -y powershell
+# Cleanup
+sudo rm packages-microsoft-prod.deb
+
 # update pip
 python -m pip install --upgrade pip
 
