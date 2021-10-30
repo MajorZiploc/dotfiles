8995dbecf235751ce51a534d4d24e369578e8932
Manyu Lakhotia <lakemonyew@gmail.com>
2021-10-30 11:14:10 -0500

    Uses path vim instead of /usr/bin/vim for env vars

--- shared/required/home/.bashrc.d/portable/env_vars.bash
+++ shared/required/home/.bashrc.d/portable/env_vars.bash
@@ -1,10 +1,10 @@
 # fc uses $FCEDIT, then $EDITOR, then vi
-export FCEDIT='/usr/bin/vim -u ~/vimfiles/rc-settings/terminal.vim'
+export FCEDIT='vim -u ~/vimfiles/rc-settings/terminal.vim'
 # <c-x> <c-e> tries to use $VISUAL, then $EDITOR, and then emacs
-export VISUAL='/usr/bin/vim -u ~/vimfiles/rc-settings/terminal.vim'
+export VISUAL='vim -u ~/vimfiles/rc-settings/terminal.vim'
 
 # the fall back to fc and <c-x> <c-e> 
-export EDITOR='/usr/bin/vim'
+export EDITOR='vim'
 
 export NODE_ENV='development'
 
