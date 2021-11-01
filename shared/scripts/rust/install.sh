761bc239b56f69c30b6982febdf62b93768c8c14
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-11-01 18:58:10 -0500

    Fixes rust install script

--- shared/scripts/rust/install.sh
+++ shared/scripts/rust/install.sh
@@ -1,13 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+. ~/.zshrc_core
 
 # install rust
-$ curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
 # install rust analyzer
 # linux - first solution worked. might have to launch into bash first when in a rust project
-# git clone https://github.com/rust-analyzer/rust-analyzer.git && cd rust-analyzer && cargo xtask install --server
+# git clone https://github.com/rust-analyzer/rust-analyzer.git;
+# cd rust-analyzer;
+# cargo xtask install --server;
+# cd ..;
+# rm -rf rust-analyzer;
+#
 # OR
+#
 # sudo snap install rust-analyzer --beta
+#
 # mac
 # brew install rust-analyzer
 
