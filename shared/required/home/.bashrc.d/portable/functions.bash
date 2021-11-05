b2c3c36bbdb9bad3eeac94c5a1f6f77e4c60e5ae
Manyu Lakhotia <lakemonyew@gmail.com>
2021-11-05 15:49:40 -0500

    Removes () from ide[hv]1 bash fns

--- shared/required/home/.bashrc.d/portable/functions.bash
+++ shared/required/home/.bashrc.d/portable/functions.bash
@@ -84,12 +84,12 @@ function tmuxds {
   _tmux_session_list_helper "$session_name" "$selected";
 }
 
-function ideh1() {
+function ideh1 {
   # splits the window into 2 panes
   tmux split-window -h -p 39;
 }
 
-function idev1() {
+function idev1 {
   # splits the window into 2 panes
   tmux split-window -v -p 30;
 }
