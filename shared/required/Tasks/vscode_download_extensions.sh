f167579bc54f9c90b29741588e383a45d74432ef
Manyu Lakhotia <lakemonyew@gmail.com>
2021-11-29 07:06:08 -0600

    Adds quokka-js and comments to vscode extension download script

--- shared/required/Tasks/vscode_download_extensions.sh
+++ shared/required/Tasks/vscode_download_extensions.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-# General plugings
-code --install-extension CoenraadS.bracket-pair-colorizer
-code --install-extension eamodio.gitlens
-code --install-extension esbenp.prettier-vscode
-code --install-extension janisdd.vscode-edit-csv
-code --install-extension mechatroner.rainbow-csv
-code --install-extension mhutchie.git-graph
-code --install-extension ms-vscode.powershell
-code --install-extension streetsidesoftware.code-spell-checker
+# Core plugings
+# vim emulation
 code --install-extension vscodevim.vim
-# See through glass feel
+# color code (square,curly)brackets,paraens
+code --install-extension CoenraadS.bracket-pair-colorizer
+# checks spelling
+code --install-extension streetsidesoftware.code-spell-checker
+# color codes csv columns
+code --install-extension mechatroner.rainbow-csv
+# code formatter for js/ts/html/json
+code --install-extension esbenp.prettier-vscode
+# advanced git actions
+code --install-extension eamodio.gitlens
+# a graph for git
+code --install-extension mhutchie.git-graph
+
+# Core-Extended plugings
+# gives a table view for data, dont recommend using it to edit data
+code --install-extension janisdd.vscode-edit-csv
+# See through glass feel 
 code --install-extension s-nlf-fh.glassit
 # Simple tmux key binds for the vscode terminal
 code --install-extension stephlin.vscode-tmux-keybinding
@@ -19,26 +28,42 @@ code --install-extension stephlin.vscode-tmux-keybinding
 # code --install-extension bajdzis.vscode-database
 
 # docker
+# run project inside the container
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-azuretools.vscode-docker
 
 # dotnet
+# base language support
 code --install-extension Ionide.Ionide-fsharp
+# base language support
 code --install-extension ms-dotnettools.csharp
+# base language support
+code --install-extension ms-vscode.powershell
 
 # Python
-code --install-extension almenon.arepl
+# base language support
 code --install-extension ms-python.python
-code --install-extension ms-toolsai.jupyter
+# intellisense
 code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+# run on save/edit
+code --install-extension almenon.arepl
+
+# javascript / typescript
+# run on save/edit
+code --install-extension wallabyjs.quokka-vscode
 
 # bash wsl
+# run inside ubuntu wsl container
 code --install-extension ms-vscode-remote.remote-wsl
+# debug bash code
 code --install-extension rogalmic.bash-debug
 
 # mssql
+# base language support
 code --install-extension ms-mssql.mssql
 
 # prolog
+# base language support
 code --install-extension arthurwang.vsc-prolog
 
