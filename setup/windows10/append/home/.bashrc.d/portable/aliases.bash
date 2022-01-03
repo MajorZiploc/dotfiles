87a8d9834e078739faa7cc0f5cbca36e57b24fa2
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2022-01-03 14:05:12 -0600

    Adds vs2019 alias to win10

--- setup/windows10/append/home/.bashrc.d/portable/aliases.bash
+++ setup/windows10/append/home/.bashrc.d/portable/aliases.bash
@@ -3,3 +3,6 @@ alias explorer="explorer.exe"
 # copy to clipboard
 alias clip="clip.exe"
 alias ifconfig='cmd_exec "ipconfig /all"'
+export VS2019="/c/Program Files (x86)/Microsoft Visual Studio/2019/Professional/Common7/IDE/devenv.exe"
+alias vs2019='eval "\"$VS2019\""'
+
