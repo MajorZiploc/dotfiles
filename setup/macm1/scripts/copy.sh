b21ac574f71b36cba2ad8ae94d09cf85de520013
Manyu Lakhotia <lakemonyew@gmail.com>
2022-01-13 09:43:22 -0600

    Adds cocinstall/update to copy scripts nonwin

--- setup/macm1/scripts/copy.sh
+++ setup/macm1/scripts/copy.sh
@@ -25,8 +25,8 @@ $SCRIPTPATH/../../../shared/scripts/copy.sh "$setupRoot" "$flags"
   done;
 }
 
-nvim "+:PlugInstall" "+:qa" 2>/dev/null
-vim "+:PlugInstall" "+:qa" 2>/dev/null
+nvim "+:PlugInstall" "+:CocInstall" "+:CocUpdate" "+:qa" 2>/dev/null
+vim "+:PlugInstall" "+:CocInstall" "+:CocUpdate" "+:qa" 2>/dev/null
 
 git restore "$SCRIPTPATH/../../../shared/scripts/create_temps.sh"
 
