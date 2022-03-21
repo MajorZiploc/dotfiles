cb64882d1c6003264be48011ca8f11a8f71eefa2
Manyu Lakhotia <lakemonyew@gmail.com>
2022-03-21 07:57:23 -0500

    + -y to add-apt repo commands

--- setup/ubuntu/scripts/install.sh
+++ setup/ubuntu/scripts/install.sh
@@ -14,7 +14,7 @@ sudo apt-get -y install python-is-python3;
 # for creating python virtual environments
 sudo apt-get -y install python3-venv;
 # ripgrep for vundle use or use on its own
-sudo add-apt-repository ppa:x4121/ripgrep;
+sudo add-apt-repository ppa:x4121/ripgrep -y;
 sudo apt-get -y update;
 sudo apt-get -y install ripgrep;
 # fuzzy finder
@@ -28,11 +28,11 @@ sudo apt-get -y install silversearcher-ag;
 # zsh
 sudo apt-get install -y zsh;
 # vim 8.2
-sudo add-apt-repository ppa:jonathonf/vim;
+sudo add-apt-repository ppa:jonathonf/vim -y;
 sudo apt-get -y update;
 sudo apt-get install -y vim;
 # neovim
-sudo add-apt-repository ppa:neovim-ppa/unstable;
+sudo add-apt-repository ppa:neovim-ppa/unstable -y;
 sudo apt-get install -y neovim;
 # ssh
 sudo apt-get remove -y openssh-client;
@@ -44,21 +44,21 @@ sudo ufw status verbose;
 sudo apt-get install -y net-tools;
 
 # prolog
-sudo add-apt-repository ppa:swi-prolog/stable
-sudo apt-get update -y
-sudo apt-get install -y swi-prolog
+sudo add-apt-repository ppa:swi-prolog/stable;
+sudo apt-get update -y;
+sudo apt-get install -y swi-prolog;
 # for vim coc prolog language server support
 swipl -q -l "$this_path/../../../shared/scripts/prolog/install_language_server.pl";
 
 # php
-sudo add-apt-repository ppa:ondrej/php
-sudo apt install -y php8.0
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
-rm composer.phar
+sudo add-apt-repository ppa:ondrej/php -y;
+sudo apt install -y php8.0;
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');";
+php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";
+php composer-setup.php;
+php -r "unlink('composer-setup.php');";
+sudo mv composer.phar /usr/local/bin/composer;
+rm composer.phar;
 
 # nodejs nvm deps
 sudo apt-get install -y build-essential checkinstall libssl-dev;
@@ -90,7 +90,7 @@ sudo apt-get -y install xclip;
 # clipboard manager, open its configs and make ctrl-` the Shortcuts -> Global -> Show the tray menu key
 sudo apt-get -y install copyq;
 # sound eq
-sudo add-apt-repository ppa:mikhailnov/pulseeffects;
+sudo add-apt-repository ppa:mikhailnov/pulseeffects -y;
 sudo apt-get -y update;
 sudo apt-get -y install pulseeffects pulseaudio --install-recommends;
 
@@ -100,7 +100,7 @@ sudo apt-get -y remove docker docker-engine docker.io containerd runc;
 sudo apt-get -y update;
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common;
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable";
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y;
 sudo apt-get -y update;
 apt-cache policy docker-ce;
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io;
@@ -115,7 +115,7 @@ sudo dpkg -i packages-microsoft-prod.deb;
 # Update the list of products
 sudo apt-get -y update;
 # Enable the "universe" repositories
-sudo add-apt-repository universe;
+sudo add-apt-repository universe -y;
 # Install PowerShell
 sudo apt-get install -y powershell;
 # Cleanup
