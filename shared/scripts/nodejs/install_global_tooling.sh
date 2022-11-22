39654425c76eb32b3f96c26b7db674a8db3f6f90
Manyu Lakhotia <mlakhotia@conservis.ag>
2022-11-22 09:43:32 -0600

    Fix nvm scripts

--- shared/scripts/nodejs/install_global_tooling.sh
+++ shared/scripts/nodejs/install_global_tooling.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 . ~/.bashrc;
-nvm install --lts;
-nvm use --lts;
+npm install --global trash-cli prettier concurrently gnomon;
+# trash-cli: safer rm, moves things to the trash rather than perm delete
+# prettier: code formatter
+# concurrently: run multiple simple commands or any kind of bash scripts at the same time
+#   useful for running multiple commands that hang a terminal at the same time with only needed 1 terminal window doing the work
+# gnomon: useful for timing an operation and deltas between prints in that operation
--- shared/scripts/nodejs/nvm_install_lts.sh
+++ shared/scripts/nodejs/nvm_install_lts.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
 . ~/.bashrc;
-npm install --global trash-cli prettier concurrently gnomon;
-# trash-cli: safer rm, moves things to the trash rather than perm delete
-# prettier: code formatter
-# concurrently: run multiple simple commands or any kind of bash scripts at the same time
-#   useful for running multiple commands that hang a terminal at the same time with only needed 1 terminal window doing the work
-# gnomon: useful for timing an operation and deltas between prints in that operation
-
+nvm install --lts;
+nvm use --lts;
