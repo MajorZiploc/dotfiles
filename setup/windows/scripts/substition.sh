f12c4b537efa951695e5798039af79886056d4da
Manyu Lakhotia <lakemonyew@gmail.com>
2022-12-10 14:35:01 -0600

    + missing ;s in win sub script

--- setup/windows/scripts/substition.sh
+++ setup/windows/scripts/substition.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )";
 
 # root of the os style configs being downloaded
-setup_root="$1"
-temp="$2"
-temp_shared="$3"
-temp_this="$4"
+setup_root="$1";
+temp="$2";
+temp_shared="$3";
+temp_this="$4";
 
-vsvimpath="$(echo "$HOME/vscodevim/_vsvimrc" | sed -E 's,^/(\w),\U\1:,g' | sed -E 's,/,\\\\\\\\,g')"
+vsvimpath="$(echo "$HOME/vscodevim/_vsvimrc" | sed -E 's,^/(\w),\U\1:,g' | sed -E 's,/,\\\\\\\\,g')";
 find "$temp_shared" -regextype egrep -iregex ".*\.json" -type f -exec sed -E -i'' "s,VSVIM_DIR_PLACEHOLDER,$vsvimpath,g" "{}" \;
 find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' 's,VIM_SHELL_PLACEHOLDER,/usr/local/bin/bash.exe,g' "{}" \;
 vim_plugin_include="Plug 'ctrlpvim/ctrlp.vim' \" fuzzy file finder";
 find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_INCLUDE_PLACEHOLDER,$vim_plugin_include,g" "{}" \;
-vim_plugset_path="so ~/vimfiles/plugin-settings/"
+vim_plugset_path="so ~/vimfiles/plugin-settings/";
 vim_plugin_settings="${vim_plugset_path}ctrlp.vim";
 find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_SETTINGS_PLACEHOLDER,$vim_plugin_settings,g" "{}" \;
 find "$temp_shared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,OS_PLACEHOLDER,windows,g" "{}" \;
@@ -29,22 +29,22 @@ extra_env_checks_placeholder="which fzy 2>\&1 2>/dev/null >/dev/null; [[ \"\$?\"
 find "$temp_shared" -regextype egrep -iregex ".*bash.*" -type f -exec sed -E -i'' "s,EXTRA_ENV_CHECKS_PLACEHOLDER,$extra_env_checks_placeholder,g" "{}" \;
 coc_plugins_placeholder="let g:coc_global_extensions=['coc-json', 'coc-pyright', 'coc-sql', 'coc-tsserver', 'coc-css', 'coc-html']";
 find "$temp_shared" -regextype egrep -iregex ".*coc.*" -type f -exec sed -E -i'' "s/COC_PLUGINS_PLACEHOLDER/$coc_plugins_placeholder/g" "{}" \;
-vim_bash_env_placeholder='"~/vimfiles/bash_env.bash"'
+vim_bash_env_placeholder='"~/vimfiles/bash_env.bash"';
 find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -E -i'' "s,VIM_BASH_ENV_PLACEHOLDER,$vim_bash_env_placeholder,g" "{}" \;
 
-unset vsvimpath
-unset vim_plugin_include
-unset vim_plugset_path
-unset vim_plugin_settings
-unset vsc_settings_destination_placeholder
-unset tmuxps_paths_array_placeholder
-unset fuzzy_finder_cdf_placeholder
-unset fuzzy_finder_placeholder
-unset extra_env_checks_placeholder
-unset coc_plugins_placeholder
-unset vim_bash_env_placeholder
-unset setup_root
-unset temp_shared
-unset temp_this
-unset temp
+unset vsvimpath;
+unset vim_plugin_include;
+unset vim_plugset_path;
+unset vim_plugin_settings;
+unset vsc_settings_destination_placeholder;
+unset tmuxps_paths_array_placeholder;
+unset fuzzy_finder_cdf_placeholder;
+unset fuzzy_finder_placeholder;
+unset extra_env_checks_placeholder;
+unset coc_plugins_placeholder;
+unset vim_bash_env_placeholder;
+unset setup_root;
+unset temp_shared;
+unset temp_this;
+unset temp;
 
