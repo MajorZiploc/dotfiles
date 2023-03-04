16b9cbde9228d40718f8ca516a78b48b5fe255d4
Manyu Lakhotia <lakemonyew@gmail.com>
2023-03-04 16:54:19 -0600

    >sudo tee inputrc

--- setup/wsl_ubuntu/scripts/disable_terminal_beep_sound.sh
+++ setup/wsl_ubuntu/scripts/disable_terminal_beep_sound.sh
@@ -3,5 +3,5 @@
 sudo sed -E -i'' '/# do not bell on tab/d' /etc/inputrc;
 sudo sed -E -i'' '/set bell-style/d' /etc/inputrc;
 
-sudo printf "# do not bell on tab-completion\nset bell-style none\nset bell-style visible\n" >> /etc/inputrc;
+sudo printf "# do not bell on tab-completion\nset bell-style none\nset bell-style visible\n" | sudo tee -a /etc/inputrc;
 
