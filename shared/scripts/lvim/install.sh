ae88465a98550d9b1e1751b51a9652bcae975f76
Manyu Lakhotia <lakemonyew@gmail.com>
2023-03-11 08:43:45 -0600

    +basic lvim setup

---
+++ shared/scripts/lvim/install.sh
@@ -0,0 +1,8 @@
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+
+# PUT THIS IN YOUR PATH:
+# export PATH="/Users/manyu/.local/bin:$PATH"
+
+# UNINSTALLER
+# bash ~/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
+
