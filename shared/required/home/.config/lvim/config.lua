bc2039e4f2a8dc082ce1ad55efee5224b500671b
Manyu Lakhotia <lakemonyew@gmail.com>
2023-03-11 17:28:00 -0600

    +css colorizer lvim

--- shared/required/home/.config/lvim/config.lua
+++ shared/required/home/.config/lvim/config.lua
@@ -203,8 +203,24 @@ lvim.plugins = {
   {"tpope/vim-fugitive"}, -- git plugin
   {"mechatroner/rainbow_csv"}, -- csv highlighter and query engine
   {"tpope/vim-obsession"}, -- self managing n?vim sessions (Session.vim w/ :Obsession <file_name.vim>?/:Obsession! (start/discard current session respectively))
-  {'eddyekofo94/gruvbox-flat.nvim'} -- color theme
+  {'eddyekofo94/gruvbox-flat.nvim'}, -- color theme
+  {
+  "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+          RGB = true, -- #RGB hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          rgb_fn = true, -- CSS rgb() and rgba() functions
+          hsl_fn = true, -- CSS hsl() and hsla() functions
+          css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          })
+  end,
+  },
 }
+
+require'colorizer'.setup()
 
 lvim.colorscheme = "gruvbox-flat"
 -- lvim.colorscheme = "tokyonight-night"
