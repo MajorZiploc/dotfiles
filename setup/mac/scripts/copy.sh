dba6acfd3a72a645022e13823ffa336608ebbeaa
Manyu Lakhotia <lakemonyew@gmail.com>
2023-03-12 16:42:38 -0500

    Make mac copy script more verbose

--- setup/mac/scripts/copy.sh
+++ setup/mac/scripts/copy.sh
@@ -10,17 +10,23 @@ flags_as_int="$((2#$flags))";
 vscode_flag_as_int="$((2#01))";
 [[ -z "$flags" ]] && { flags='00'; }
 
-"$script_path/../../../shared/scripts/copy.sh" "$setup_root" "$flags" 2>&1 | grep 'No such file or directory';
+echo "Running shared copy script... Output will be shown at the end all at once";
+copy_script_output=$("$script_path/../../../shared/scripts/copy.sh" "$setup_root" "$flags" 2>&1);
+did_copy_fail=$(echo "$copy_script_output" | grep 'No such file or directory');
 did_copy_fail=$?;
 
 [[ "$did_copy_fail" == "0" ]] && {
+  echo "Failure shared copy output: $copy_script_output";
   echo "Looks like you dont have coreutils (linux polyfills) installed or sourced in your rc file"
   echo "  Trying backup method..."
   create_shared_temps_to_replace='cp -r "\$script_path/../../shared/" "\$temp/"';
   create_shared_temps_replace_with='cp -r "\$script_path/../../shared/" "\$temp_shared/"';
   gsed -E -i'' "s,$create_shared_temps_to_replace,$create_shared_temps_replace_with,g" "$script_path/../../../shared/scripts/create_temps.sh";
-  "$script_path/../../../shared/scripts/copy.sh" "$setup_root" "$flags" 2>&1 | grep 'No such file or directory';
+  copy_script_output=$("$script_path/../../../shared/scripts/copy.sh" "$setup_root" "$flags" 2>&1);
 }
+
+echo "$copy_script_output";
+echo "Finished shared copy script";
 
 [[ $(($vscode_flag_as_int & $flags_as_int)) == $vscode_flag_as_int ]] && {
   vscode_dir="$HOME/.vscoderc.d/";
@@ -41,6 +47,7 @@ fi
 
 git restore "$script_path/../../../shared/scripts/create_temps.sh";
 
+unset copy_script_output;
 unset did_copy_fail;
 unset setup_root;
 
