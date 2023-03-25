fb17200a95630e39bf9279e627ba83147de62279
Manyu Lakhotia <lakemonyew@gmail.com>
2023-03-25 18:25:18 -0500

    +update_git_repos script

--- shared/scripts/routine_update.sh
+++ shared/scripts/routine_update.sh
@@ -3,9 +3,10 @@
 scriptpath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )";
 
 function main {
-  omz update;
   "$scriptpath/update_vims.sh";
   "$scriptpath/update_packages.sh";
+  "$scriptpath/update_git_repos.sh";
+  omz update;
 }
 
 main
