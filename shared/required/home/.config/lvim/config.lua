66ad80657ccdfdbe2933acaa026fed067fc7f1d5
Manyu Lakhotia <lakemonyew@gmail.com>
2023-04-08 06:49:35 -0500

    ~<leader>gd->Telescope lsp_definitions

--- shared/required/home/.config/lvim/config.lua
+++ shared/required/home/.config/lvim/config.lua
@@ -201,6 +201,7 @@ lvim.builtin.which_key.mappings["g"] = {
 lvim.lsp.buffer_mappings.normal_mode['<leader>wi'] = { vim.lsp.buf.hover, "Show hover" }
 lvim.lsp.buffer_mappings.normal_mode['<leader>gq'] = { vim.lsp.buf.hover, "Show hover" }
 lvim.lsp.buffer_mappings.normal_mode['<leader>gd'] = lvim.lsp.buffer_mappings.normal_mode['gd']
+lvim.lsp.buffer_mappings.normal_mode['<leader>gd'] = { "<CMD>Telescope lsp_definitions<CR>", "Go to definitions" }
 -- basic single line quickfix list style view of references
 -- lvim.lsp.buffer_mappings.normal_mode['<leader>gr'] = lvim.lsp.buffer_mappings.normal_mode['gr']
 -- Uses telescope to show all references and the context/file they exist in
