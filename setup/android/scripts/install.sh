f1670dd821f9702868250ff73b19db0ea3d0b2d6
Manyu Lakhotia <lakemonyew@gmail.com>
2023-04-08 20:42:46 -0500

    ^install script

--- setup/android/scripts/install.sh
+++ setup/android/scripts/install.sh
@@ -2,32 +2,31 @@
 
 this_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )";
 
-pkg -y update;
+# This block is all fine
+pkg install root-repo;
+pkg upgrade;
+pkg update -y;
 # session manager
-pkg -y install tmux;
+pkg install -y tmux;
 # for cr formatting between dos and unix. gives dos2unix and unix2dos
-pkg -y install dos2unix;
+pkg install -y dos2unix;
+pkg install -y git;
+# vim
+pkg install -y vim-python;
+# neovim
+pkg install -y neovim;
+pkg install -y zsh;
+
+# TODO: verifty fzf works here
 # fuzzy finder
 # make sure this isnt around (its to old for the vim plugin)
-pkg -y remove fzf;
+pkg remove -y fzf;
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf;
 sed -E -i'' 's,curl -[^[:blank:]]+,curl -0Lk,g' ~/.fzf/install;
 ~/.fzf/install;
+
 # for Ag in vim
-pkg -y install silversearcher-ag;
-# ssh
-pkg remove -y openssh-client;
-pkg install -y openssh-client;
-pkg remove -y openssh-server;
-pkg install -y openssh-server;
-ufw allow ssh;
-ufw status verbose;
-pkg install -y net-tools;
+pkg install -y silversearcher-ag;
 
-# update pip
-python -m pip install --upgrade pip;
-
-# vim with clipboard support
-pkg -y remove vim-common vim-runtime vim;
-pkg -y install --upgrade vim-common vim-runtime vim;
-pkg -y install vim-gtk;
+pkg install -g nodejs;
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash;
