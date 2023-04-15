7dc05d6e6845baf384f3118be2a46ac636506e72
Manyu Lakhotia <lakemonyew@gmail.com>
2023-04-15 15:35:02 -0500

    Narrow notify filter condition

--- shared/required/home/.config/lvim/config.lua
+++ shared/required/home/.config/lvim/config.lua
@@ -526,7 +526,10 @@ dap.configurations.sh = {
 --   This feature will be removed in Nvim version 0.10
 local orig_notify = vim.notify
 local filter_notify = function(text, level, opts)
-  if type(text) == "string" and (string.find(text, ":help deprecated", 1, true) or string.find(text, "stack trace", 1, true)) then
+  -- more specific to this case
+  if type(text) == "string" and (string.find(text, "get_query", 1, true) or string.find(text, "get_node_text", 1, true)) then
+  -- for all deprecated and stack trace warnings
+  -- if type(text) == "string" and (string.find(text, ":help deprecated", 1, true) or string.find(text, "stack trace", 1, true)) then
     return
   end
   orig_notify(text, level, opts)
