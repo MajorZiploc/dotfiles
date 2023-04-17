663c20bdb5bc0493abac424f79e36d877209d469
Manyu Lakhotia <mlakhotia@conservis.ag>
2023-04-17 11:23:53 -0500

    Disable auto launch of firenvim on textareas

--- shared/required/home/vimfiles/plugin-settings/firenvim.lua
+++ shared/required/home/vimfiles/plugin-settings/firenvim.lua
@@ -1,3 +1,4 @@
 if vim.g.started_by_firenvim == true then
   vim.cmd('set guifont=monospace:h20')
+  vim.cmd("let g:firenvim_config={'globalSettings':{},'localSettings':{'.*':{ 'takeover': 'never' }}}")
 end
