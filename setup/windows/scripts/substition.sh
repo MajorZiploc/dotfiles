3a3aed1e49bc39b62da49215b10bba148f3eb04d
Manyu Lakhotia <mlakhotia@conservis.ag>
2023-04-17 11:01:57 -0500

    Increase firenvim font

--- setup/windows/scripts/substition.sh
+++ setup/windows/scripts/substition.sh
@@ -16,8 +16,8 @@ vim_plugin_include="${vim_plugin_include}\nif has\('nvim'\)\n  Plug 'eddyekofo94
 find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_INCLUDE_PLACEHOLDER,$vim_plugin_include,g" "{}" \;
 vim_plugset_path="so ~/vimfiles/plugin-settings/";
 vim_plugin_settings="${vim_plugset_path}ctrlp.vim";
-vim_plugin_settings="$vim_plugin_settings\nif has\('nvim'\)\n  ${vim_plugset_path}gruvbox_flat.vim\nendif";
 # nvim specific plugins
+vim_plugin_settings="$vim_plugin_settings\nif has\('nvim'\)\n  ${vim_plugset_path}gruvbox_flat.vim\n  ${vim_plugset_path}firenvim.lua\nendif";
 find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' "s,VIM_PLUGIN_SETTINGS_PLACEHOLDER,$vim_plugin_settings,g" "{}" \;
 find "$temp_shared" -regextype egrep -iregex ".*" -type f -exec sed -E -i'' "s,OS_PLACEHOLDER,windows,g" "{}" \;
 vsc_settings_destination_placeholder="\$HOME/AppData/Roaming/Code/User/";
---
+++ shared/required/home/vimfiles/plugin-settings/firenvim.lua
@@ -0,0 +1,3 @@
+if vim.g.started_by_firenvim == true then
+  vim.cmd('set guifont=monospace:h20')
+end
