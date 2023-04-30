6970ff647278feee5d37aaa79f2c0588d4263dc2
Manyu Lakhotia <lakemonyew@gmail.com>
2023-04-30 12:25:28 -0500

    +indent_4_to_2,indent_2_to_4

--- shared/required/home/.bashrc.d/portable/aliases.bash
+++ shared/required/home/.bashrc.d/portable/aliases.bash
@@ -30,6 +30,8 @@ alias keep_first='cat | awk "!x[\$0]++" | cat';
 alias add_semicolons='sed -E "s/(.+)/\1;/;s/;;$/;/;"';
 alias to_title_case='sed -E "s/\b(.)/\u\1/g"';
 alias to_less_blank_lines='perl -00 -pe '"'"'s/\n{2,}(?=[^\n])/"\n"/g'"'";
+alias indent_4_to_2='perl -pe '"'"'s{^((?: {4})*)}{" " x (2*length($1)/4)}e'"'"''
+alias indent_2_to_4='perl -pe '"'"'s{^((?: {2})*)}{" " x (4*length($1)/2)}e'"'"''
 
 # Converts a string to a fuzzy search pattern
 alias to_fuzz='sed -E "s/(\\w)/\\1\[^\[:blank:\]\]{0,3}/g" | sed "s/\[^\[:blank:\]\]{0,3}$//g"';
