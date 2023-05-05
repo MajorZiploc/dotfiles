b8b7a0f9f5282b21c45f684eb4ee0d8c6a21e5d1
Manyu Lakhotia <mlakhotia@conservis.ag>
2023-05-05 11:42:43 -0500

    !scriptpath var in update packages

--- shared/scripts/update_packages.sh
+++ shared/scripts/update_packages.sh
@@ -3,9 +3,9 @@
 scriptpath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )";
 
 function main {
-  "$script_path/nodejs/install_global_tooling.sh" "update";
-  "$script_path/python/packages.sh" "-U";
-  "$script_path/rust/packages.sh" "update";
+  "$scriptpath/nodejs/install_global_tooling.sh" "update";
+  "$scriptpath/python/packages.sh" "-U";
+  "$scriptpath/rust/packages.sh" "update";
 }
 
 main
