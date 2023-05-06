e459c308b008449d50ed5c53324939a2a5dd4b2c
Manyu Lakhotia <lakemonyew@gmail.com>
2023-05-06 09:44:11 -0500

    -lir overrides to netrw

--- shared/scripts/lvim/adjust_lvim_configs.sh
+++ shared/scripts/lvim/adjust_lvim_configs.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 sed -E -i'' 's/(vim.cmd \{ cmd = "TSInstall", args = missing, bang = true \})/-- \1/g' ~/.local/share/lunarvim/lvim/lua/lvim/core/treesitter.lua;
+
+rm ~/.local/share/lunarvim/site/pack/lazy/opt/lir.nvim/plugin/lir.vim;
+
