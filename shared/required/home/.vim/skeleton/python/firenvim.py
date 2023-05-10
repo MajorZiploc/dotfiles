3be781344e012052fc3904b57abb743fdd346a4c
Manyu Lakhotia <mlakhotia@conservis.ag>
2023-05-10 15:15:09 -0500

    +firenvim.py import template

---
+++ shared/required/home/.vim/skeleton/python/firenvim.py
@@ -0,0 +1,50 @@
+import functools as ft
+# groupby is pretty terrible from itertools
+from itertools import permutations, count, chain
+import re
+import os
+import time
+import sys
+from time import sleep, strftime
+from datetime import datetime
+from queue import Queue
+from queue import LifoQueue
+from collections import defaultdict, deque, OrderedDict, Counter, namedtuple
+from heapq import heappop, heappush, heapify
+import copy
+import random
+import math
+import timeit
+# shell scripts and bash commands
+# import sh
+from enum import Enum
+import gzip
+# serialization and deserialization
+import pickle
+# for abstract classes
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Callable, Iterator, Union, Optional, Any, cast, Mapping, MutableMapping, Sequence, Iterable, Match, AnyStr, IO, TypeVar, List, Set, Dict, Tuple
+# https://docs.python.org/3/library/asyncio-task.html
+import asyncio
+from collections import OrderedDict
+import datetime
+import json
+import logging
+import uuid
+import pytz
+# from dateutil import parser as date_parser, tz
+import dateutil
+import signal
+import pandas
+from sentry_sdk import capture_message
+from django.db import models
+# from django.utils.timezone import now
+import django.utils.timezone
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+from django.db.models import CASCADE, F, Max, TextField, Value
+from django.db.models.functions import Concat
+from django.http import HttpResponse
+from django.http import JsonResponse
+
