fc38e6afaaa01d1f0ec0038d75deadf0a73055aa
Manyu Lakhotia <lakemonyew@gmail.com>
2023-05-13 16:31:57 -0500

    +ts-node global npm package

--- shared/scripts/nodejs/install_global_tooling.sh
+++ shared/scripts/nodejs/install_global_tooling.sh
@@ -8,7 +8,9 @@ function main {
   # concurrently: run multiple simple commands or any kind of bash scripts at the same time
   #   useful for running multiple commands that hang a terminal at the same time with only needed 1 terminal window doing the work
   # gnomon: useful for timing an operation and deltas between prints in that operation
-  local packages="trash-cli prettier concurrently gnomon sql-formatter-cli;";
+  # sql-formatter-cli: formats sql code
+  # ts-node: runs typescript files the same as node runs javascript files
+  local packages="trash-cli prettier concurrently gnomon sql-formatter-cli ts-node;";
   eval "npm $method --global $packages";
 }
 
