89e2f3cef97cf6f51b4d825053b22b1f2f112b71
Manyu Lakhotia <lakemonyew@gmail.com>
2023-09-09 16:12:04 -0500

    -lvim update attempt

--- shared/scripts/update_vims.sh
+++ shared/scripts/update_vims.sh
@@ -3,10 +3,10 @@
 # NOTE: NOT PERFECT - THERE IS NO GOOD WAY TO KNOW WHEN A COMMAND IS DONE. SO A CHAIN OF COMMANDS LIKE THIS MAY NOT WORK
 
 function main {
-  if which lvim; then
-    # TODO: error here; this doesnt work, most update yourself in lvim for now
-    lvim --headless "+Lazy! sync" +qa;
-  fi
+  # if which lvim; then
+  #   # TODO: error here; this doesnt work, most update yourself in lvim for now
+  #   lvim --headless "+Lazy! sync" +qa;
+  # fi
   if which nvim; then
     nvim "+:PlugInstall" "+:PlugUpgrade" "+:PlugUpdate" "+:CocInstall" "+:CocUpdate" 2>/dev/null;
   fi
