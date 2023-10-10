9e0fe6fd7987e92e324a637cce6e6a7cedd9a0c3
Manyu Lakhotia <lakemonyew@gmail.com>
2023-10-10 16:00:59 -0500

    !sh git cloner

--- shared/required/Tasks/git_download_projs.sh
+++ shared/required/Tasks/git_download_projs.sh
@@ -1,16 +1,16 @@
 # projects_path="./projs/bitbucket_clone.csv";
 projects_path="./projs/github_repos_clone.csv";
 projects=($(rbql --with-header --query "Select distinct a.clone" --delim ',' --policy quoted_rfc --input "$projects_path" | tail -n +2));
-$root_path="~/projects"
-cd "$root_path"
+root_path="~/projects";
+cd "$root_path";
 
 for project in ${projects[@]}; do
   # echo "$project" hi;
 
   # bitbucket
-  # eval "$_.clone";
+  # eval "$project";
 
   # github
-  git clone $_.clone;
+  git clone "$project";
 done;
 
