cee9a3cf955913558d8df2020890839de767df72
Manyu Lakhotia <mlakhotia@conservis.ag>
2023-10-17 08:02:08 -0500

    Rn wallpaper

--- shared/required/home/.wezterm.lua
+++ shared/required/home/.wezterm.lua
@@ -22,7 +22,7 @@ local function get_wallpaper()
 	-- end
 	return {
 		-- source = { File = { path = get_random_entry(wallpapers) } },
-		source = { File = os.getenv("HOME") .. "/Pictures/Wallpapers/church_flowers_girl_kneel_guy_stand_side_shot.jpg", },
+		source = { File = os.getenv("HOME") .. "/Pictures/Wallpapers/church_flowers_girl_01.jpg", },
 		height = "Cover",
 		width = "Cover",
 		horizontal_align = "Center",
