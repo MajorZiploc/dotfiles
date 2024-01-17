ed0a1a4da1180f37365e0681abd2cdda6d25fa19
Manyu Lakhotia <mlakhotia@conservis.ag>
2024-01-17 07:46:15 -0600

    +VSCODE_EXTRA_KEYBINDINGS_PLACEHOLDER

--- setup/windows/scripts/substition.sh
+++ setup/windows/scripts/substition.sh
@@ -11,6 +11,8 @@ temp_this="$4";
 vsvimpath="$(echo "$HOME/vscodevim/_vsvimrc" | sed -E 's,^/(\w),\U\1:,g' | sed -E 's,/,\\\\\\\\,g')";
 find "$temp_shared" -regextype egrep -iregex ".*\.json" -type f -exec sed -E -i'' "s,VSVIM_DIR_PLACEHOLDER,$vsvimpath,g" "{}" \;
 find "$temp_shared" -regextype egrep -iregex ".*\.json" -type f -exec sed -E -i'' "s,HOME_DIR_PLACEHOLDER,$HOME,g" "{}" \;
+vscode_extra_keybindings_placeholder=',{ "key": "ctrl+f ctrl+b", "command": "workbench.action.quickOpenPreviousRecentlyUsedEditor" }';
+find "$temp_shared" -regextype egrep -iregex ".*\.json" -type f -exec gsed -E -i'' "s/VSCODE_EXTRA_KEYBINDINGS_PLACEHOLDER/${vscode_extra_keybindings_placeholder}/g" "{}" \;
 find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -i'' 's,VIM_SHELL_PLACEHOLDER,~/bin/bash.exe,g' "{}" \;
 remove_coc="\bcoc\b";
 find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -E -i'' "/$remove_coc/d" "{}" \;
@@ -38,6 +40,7 @@ find "$temp_shared" -regextype egrep -iregex ".*vim.*" -type f -exec sed -E -i''
 
 unset vsvimpath;
 unset vim_plugin_include;
+unset vscode_extra_keybindings_placeholder
 unset vim_plugset_path;
 unset vim_plugin_settings;
 unset vsc_settings_destination_placeholder;
