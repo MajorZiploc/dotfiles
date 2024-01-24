e5e43e2be797f2c6dae3f30252f698cb10ff61ab
Manyu Lakhotia <lakemonyew@gmail.com>
2024-01-24 13:21:01 -0600

    !git clone process

--- shared/required/Tasks/git_download_projs.sh
+++ shared/required/Tasks/git_download_projs.sh
@@ -6,11 +6,11 @@ origin_style="github";
 # Optional: cleaning step
 cat "$projects_path_dirty" | rbql --with-header --query "Select distinct a.clone where '${origin_style}' in a.clone" --delim ',' --policy quoted_rfc > "${projects_path}";
 
-projects=($(rbql --with-header --query "Select distinct a.clone" --delim ',' --policy quoted_rfc --input "$projects_path" | tail -n +2));
-root_path="~/projects";
+projects=$(rbql --with-header --query "Select distinct a.clone" --delim ',' --policy quoted_rfc --input "$projects_path" | tail -n +2);
+root_path="$HOME/projects";
 cd "$root_path";
 
-for project in ${projects[@]}; do
+for project in ${projects}; do
   # echo "$project" hi;
 
   # bitbucket
