8068f141c995e982d2506958652af71734c5576b
Manyu Lakhotia <lakemonyew@gmail.com>
2024-09-12 08:47:29 -0500

    ^set_preferences.ps1

--- setup/windows/scripts/set_preferences.ps1
+++ setup/windows/scripts/set_preferences.ps1
@@ -1,7 +1,7 @@
 # Set the shortest keyboard repeat delay (0 is the shortest)
-Set-ItemProperty -Path 'HKCU:\Control Panel\Keyboard' -Name "KeyboardDelay" -Value 0
+Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardDelay" -Value 0
 # Set the fastest keyboard repeat rate (2 is the fastest)
-Set-ItemProperty -Path 'HKCU:\Control Panel\Keyboard' -Name "KeyboardSpeed" -Value 2
+Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardSpeed" -Value 2
 
 # to view current property values
 # get-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardDelay"
