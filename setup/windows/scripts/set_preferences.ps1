5b3644ffe75e49c1a8ed05abc46475010eb5ae55
Manyu Lakhotia <lakemonyew@gmail.com>
2024-09-12 13:32:45 -0500

    ^keyboard speed win

--- setup/windows/scripts/set_preferences.ps1
+++ setup/windows/scripts/set_preferences.ps1
@@ -1,8 +1,16 @@
 # Set the shortest keyboard repeat delay (0 is the shortest)
 Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardDelay" -Value 0
-# Set the fastest keyboard repeat rate (2 is the fastest)
-Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardSpeed" -Value 2
+# Set the fastest keyboard repeat rate (31 is the fastest i guess)
+Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardSpeed" -Value 100
 
 # to view current property values
 # get-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardDelay"
 # get-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardSpeed"
+#
+Set-Location "HKCU:\Control Panel\Accessibility\Keyboard Response"
+
+Set-ItemProperty -Path . -Name AutoRepeatDelay       -Value 200
+Set-ItemProperty -Path . -Name AutoRepeatRate        -Value 30
+Set-ItemProperty -Path . -Name DelayBeforeAcceptance -Value 0
+Set-ItemProperty -Path . -Name BounceTime            -Value 0
+Set-ItemProperty -Path . -Name Flags                 -Value 47
