c4ed9d1d029d7d31098650919769ef88fe77405c
Manyu Lakhotia <lakemonyew@gmail.com>
2024-10-09 09:31:24 -0500

    Convert justdecompile commands to bash

--- setup/wsl_ubuntu/append/home/.bashrc.d/portable/functions.bash
+++ setup/wsl_ubuntu/append/home/.bashrc.d/portable/functions.bash
@@ -44,3 +44,31 @@ function cdwh {
   fi
 }
 
+function decompile_dlls_and_exes {
+  local folder_to_decompile="$1";
+  local output_folder_in_windows_absolute_path_style="$2";
+  # JustDecompile location on windows
+  # /mnt/c/Program Files (x86)/Progress/JustDecompile/Libraries/JustDecompile.exe
+  [[ -z "$folder_to_decompile" ]] && { echo "Must specify folder_to_decompile" >&2; return 1; }
+  [[ -z "$output_folder_in_windows_absolute_path_style" ]] && { echo "Must specify output_folder_in_windows_absolute_path_style" >&2; return 1; }
+  mkdir -p "$output_folder_in_windows_absolute_path_style";
+  find "${folder_to_decompile}" -regextype egrep -iregex ".*\.(dll|exe)" -type f -print0 | while read -d $'\0' file; do
+    win_file=`realpath "$file" | sed -E "s,^/(\w)/,\U\1:/,g"`;
+    d=`basename "$file" | sed -E 's,(\S+)\.(dll|exe),\1_\2,'`;
+    cmd.exe "/c JustDecompile.exe /out:${output_folder_in_windows_absolute_path_style}/$d /target:$win_file";
+    echo "Done with $win_file";
+  done;
+}
+
+function type_info_for_cs {
+  local folder_with_decompiled_content="$1";
+  local output_folder="$2";
+  [[ -z "$folder_with_decompiled_content" ]] && { echo "Must specify folder_with_decompiled_content" >&2; return 1; }
+  [[ -z "$output_folder" ]] && { echo "Must specify output_folder" >&2; return 1; }
+  find "${folder_with_decompiled_content}" -regextype egrep -iregex ".*(dll|exe)" -type d -print0 | while read -d $'\0' folder; do
+    abs_folder_name=`realpath "$folder"`;
+    base_folder_name=`basename "$folder"`;
+    find "$abs_folder_name" -regextype egrep -iregex ".*\.cs" -type f -not -path '*/.git/*' -not -path '*/.svn/*' -not -path '*/node_modules/*' -not -path '*/.venv/*' -exec egrep --color -in -e "\b(private|internal|public|class|enum|interface)\b" "{}" + | tee "${output_folder}/${base_folder_name}.txt";
+  done;
+}
+
