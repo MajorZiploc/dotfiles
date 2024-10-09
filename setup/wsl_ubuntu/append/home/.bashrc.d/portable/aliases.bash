6958c22c570a37dea149406b0e860c6f3a34c039
Manyu Lakhotia <lakemonyew@gmail.com>
2024-10-09 09:56:34 -0500

    !alias file

--- setup/wsl_ubuntu/append/home/.bashrc.d/portable/aliases.bash
+++ setup/wsl_ubuntu/append/home/.bashrc.d/portable/aliases.bash
@@ -7,5 +7,4 @@ alias vim='nvim'
 alias pwd_wsl='echo "//wsl.localhost/Ubuntu`pwd`" | to_winpath'
 
 alias to_winpath_wsl='sed -E "s,^/home,//wsl.localhost/Ubuntu/home,;s,^/mnt,,;s,^/(\w)/,\U\1:/,g" | sed s,/,\\\\,g';
-\\wsl.localhost\Ubuntu\home\majorziploc\projects\background_worker.fs\bin\Debug\net8.0
 alias to_unixpath_wsl='sed -E "s,^(\w):,/mnt/\L\1,g" | sed s,\\\\,/,g | sed -E "s,^//wsl.localhost/Ubuntu/home,/home,g"';
